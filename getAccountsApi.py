bd9944da6e8d0511a498d903af6b29526e016add
Sakthi Murugan <sakthimurugan@Igloos-MacBook-Pro.local>
2018-08-30 21:59:29 +0100

    DMRE-22 Handled null values in response Json

--- getAccountsApi.py
+++ getAccountsApi.py
@@ -53,46 +53,63 @@ def extract_meter_point_json(data, account_id, k):
     Writing into meter_points.csv , registers.csv, meteres.csv, attributes.csv
     key = meter_point_id
     '''
+    meter_point_ids = []
 
     meta_meters = ['associationStartDate', 'associationEndDate', 'supplyStartDate', 'supplyEndDate', 'isSmart', 'isSmartCommunicating', 'id', 'meterPointNumber', 'meterPointType']
-    df_meterpoints = json_normalize(data, record_path=['meters'], meta=meta_meters)
-    df_meterpoints['account_id'] = account_id
-    df_meterpoints1 = df_meterpoints[meta_meters + ['account_id']]
-    df_meterpoints1.rename(columns={'id' : 'meter_point_id'}, inplace=True)
-    # df_meterpoints1.to_csv('meter_points_' + str(account_id) + '_.csv')
-    df_meter_points_string = df_meterpoints1.to_csv(None, index=False)
-    file_name_meterpoints = 'meter_points_' + str(account_id) + '.csv'
-    k.key = 'ensek-meterpoints/MeterPoints/' + file_name_meterpoints
-    k.set_contents_from_string(df_meter_points_string)
-    
+    df_meterpoints = json_normalize(data)
+    # print(df_meterpoints['associationEndDate'])
+    if(df_meterpoints.empty):
+        print(" - has no meters points data")
+    else:
+        df_meterpoints['account_id'] = account_id
+        df_meterpoints2 = df_meterpoints[meta_meters + ['account_id']]
+        df_meterpoints1 = df_meterpoints2.rename(columns={'id' : 'meter_point_id'})
+        meter_point_ids = df_meterpoints1['meter_point_id']
+        # df_meterpoints1.to_csv('meter_points_' + str(account_id) + '_.csv')
+        df_meter_points_string = df_meterpoints1.to_csv(None, index=False)
+        file_name_meterpoints = 'meter_points_' + str(account_id) + '.csv'
+        k.key = 'ensek-meterpoints/MeterPoints/' + file_name_meterpoints
+        k.set_contents_from_string(df_meter_points_string)
+
+
     meta_meters = ['meterId', 'meterSerialNumber', 'installedDate', 'removedDate', 'meter_point_id']
     df_meters = json_normalize(data, record_path=['meters'], meta=['id'], meta_prefix='meter_point_')
-    df_meters['account_id'] = account_id
-    df_meters1 = df_meters[meta_meters + ['account_id']]
-    # df_meters1.to_csv('meters_'  + str(account_id) + '.csv')
-    df_meters_string = df_meters1.to_csv(None, index=False)
-    filename_meters = 'meters_' + str(account_id) + '.csv'
-    k.key = 'ensek-meterpoints/Meters/' + filename_meters
-    k.set_contents_from_string(df_meters_string)
+    if(df_meters.empty):
+        print(" - has no meters data")
+    else:
+        df_meters['account_id'] = account_id
+        df_meters1 = df_meters[meta_meters + ['account_id']]
+        # df_meters1.to_csv('meters_'  + str(account_id) + '.csv')
+        df_meters_string = df_meters1.to_csv(None, index=False)
+        filename_meters = 'meters_' + str(account_id) + '.csv'
+        k.key = 'ensek-meterpoints/Meters/' + filename_meters
+        k.set_contents_from_string(df_meters_string)
+    
 
     df_attributes = json_normalize(data, record_path=['attributes'], record_prefix='attributes_', meta=['id'], meta_prefix='meter_point_')
-    df_attributes['account_id'] = account_id
-    # df_attributes.to_csv('attributes_'  + str(account_id) + '.csv')
-    df_attributes_string = df_attributes.to_csv(None, index=False)
-    filename_attributes =  'attributes_'  + str(account_id) + '.csv'
-    k.key = 'ensek-meterpoints/Attributes/' + filename_attributes
-    k.set_contents_from_string(df_attributes_string)
+    if(df_attributes.empty):
+        print(" - has no attributes data")
+    else:
+        df_attributes['account_id'] = account_id
+        # df_attributes.to_csv('attributes_'  + str(account_id) + '.csv')
+        df_attributes_string = df_attributes.to_csv(None, index=False)
+        filename_attributes =  'attributes_'  + str(account_id) + '.csv'
+        k.key = 'ensek-meterpoints/Attributes/' + filename_attributes
+        k.set_contents_from_string(df_attributes_string)
 
     df_registers = json_normalize(data, record_path=['meters','registers'],meta=['id'], 
     meta_prefix='meter_point_', record_prefix='registers_')
-    df_registers['account_id'] = account_id
-    # df_registers.to_csv('registers_' + str(account_id) + '.csv')
-    df_registers_string = df_registers.to_csv(None, index=False)
-    filename_registers =  'registers_'  + str(account_id) + '.csv'
-    k.key = 'ensek-meterpoints/Registers/' + filename_registers
-    k.set_contents_from_string(df_registers_string)
+    if(df_registers.empty):
+        print(" - has no registers data")
+    else:
+        df_registers['account_id'] = account_id
+        # df_registers.to_csv('registers_' + str(account_id) + '.csv')
+        df_registers_string = df_registers.to_csv(None, index=False)
+        filename_registers =  'registers_'  + str(account_id) + '.csv'
+        k.key = 'ensek-meterpoints/Registers/' + filename_registers
+        k.set_contents_from_string(df_registers_string)
 
-    return df_meterpoints1['meter_point_id']
+    return meter_point_ids 
 
 def extract_meter_readings_json(data, account_id, meter_point_id,k):
     meta_readings = ['id', 'readingType', 'meterPointId', 'dateTime', 'createdDate', 'meterReadingSource']
@@ -124,8 +141,8 @@ def extract_meter_readings_json(data, account_id, meter_point_id,k):
 def get_S3_Connections():
     access_key = con.s3_config['access_key']
     secret_key = con.s3_config['secret_key']
-    print(access_key)
-    print(secret_key)
+    # print(access_key)
+    # print(secret_key)
 
     s3 = boto.connect_s3(aws_access_key_id=access_key, aws_secret_access_key=secret_key)
     bucket = s3.get_bucket('igloo-uat-bucket')
@@ -142,32 +159,38 @@ def get_Users(k):
     # print(len(p))
     return p
 
+def format_json_response(data):
+    data_str = json.dumps(data, indent=4).replace('null','""')
+    data_json = json.loads(data_str)
+    return data_json
 
 def main():
-    # account_ids = con.api_config['account_ids']
+    account_ids = con.api_config['account_ids']
     t = con.api_config['total_no_of_calls']
 
     k = get_S3_Connections()
 
-    account_ids = get_Users(k)
+    # account_ids = get_Users(k)
+
     # run for configured account ids
     for account_id in account_ids[:t]:
         api_url,token,head = get_meter_point_api_info(account_id)
         meter_info_response = get_api_response(api_url,token,head)
         if(meter_info_response):
-            meter_points = extract_meter_point_json(meter_info_response, account_id, k)
             print('ac:' + str(account_id))
+            formatted_meter_info = format_json_response(meter_info_response)
+            meter_points = extract_meter_point_json(formatted_meter_info, account_id, k)
             for each_meter_point in meter_points:
                 api_url,token,head = get_meter_readings_api_info(account_id, each_meter_point)
                 meter_reading_response = get_api_response(api_url,token,head)
                 if(meter_reading_response):
-                    extract_meter_readings_json(meter_reading_response, account_id, each_meter_point,k)
                     print('mp:' + str(each_meter_point))
+                    formatted_meter_reading = format_json_response(meter_reading_response)
+                    extract_meter_readings_json(formatted_meter_reading, account_id, each_meter_point,k)
                 else:
-                    print('mp:' + account_id + ' has no data')
-                    
+                    print('mp:' + str(each_meter_point) + ' has no data' )
         else:
-            print('ac:' + account_id + ' has no data')
+            print('ac:' + str(account_id) + 'has no data')
 
 if __name__ == "__main__":
     main()
