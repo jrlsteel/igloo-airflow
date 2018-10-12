2a7c0e7e15b3dd61581d00d9b15b0c5cd6c05e70
sakthimurugan <sakthi.murugan@igloo.energy>
2018-10-12 18:06:14 +0100

    DMRE-72

--- processAllEnsekScripts.py
+++ processAllEnsekScripts.py
@@ -4,4 +4,6 @@ os.system('python processEnsecApiData.py')
 os.system('python processEnsekApiInternalReadingsData.py')
 os.system('python processEnsekApiInternalEstimatesData.py')
 os.system('python processEnsekApiTariffsWithHistory.py')
+os.system('python processEnsekApiDirectDebits.py')
+os.system('python processEnsekApiStatusRegistrationsData.py')
 
