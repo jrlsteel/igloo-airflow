92ad2cbd7a1b52abc410720f4e6b11183ff6427f
sakthimurugan <sakthi.murugan@igloo.energy>
2018-10-12 18:13:21 +0100

    DMRE-72: Changed the os command from python to python3

--- processAllEnsekScripts.py
+++ processAllEnsekScripts.py
@@ -1,9 +1,9 @@
 import os
 #Process All Ensek data
-os.system('python processEnsecApiData.py')
-os.system('python processEnsekApiInternalReadingsData.py')
-os.system('python processEnsekApiInternalEstimatesData.py')
-os.system('python processEnsekApiTariffsWithHistory.py')
-os.system('python processEnsekApiDirectDebits.py')
-os.system('python processEnsekApiStatusRegistrationsData.py')
+os.system('python3 processEnsecApiData.py')
+os.system('python3 processEnsekApiInternalReadingsData.py')
+os.system('python3 processEnsekApiInternalEstimatesData.py')
+os.system('python3 processEnsekApiTariffsWithHistory.py')
+os.system('python3 processEnsekApiDirectDebits.py')
+os.system('python3 processEnsekApiStatusRegistrationsData.py')
 
