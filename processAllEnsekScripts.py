377c9da02ac9f9202fa9d1f2e205cfe6cf80877a
Jonathan Steel <jonathan.steel@igloo.energy>
2018-10-16 11:39:09 +0100

    DMRE-72

--- processAllEnsekScripts.py
+++ processAllEnsekScripts.py
@@ -1,9 +1,10 @@
 import os
 #Process All Ensek data
-os.system('python3 processEnsecApiData.py')
-os.system('python3 processEnsekApiInternalReadingsData.py')
-os.system('python3 processEnsekApiInternalEstimatesData.py')
-os.system('python3 processEnsekApiTariffsWithHistory.py')
-os.system('python3 processEnsekApiDirectDebits.py')
-os.system('python3 processEnsekApiStatusRegistrationsData.py')
+while True:
+    os.system('python3 processEnsecApiData.py')
+    os.system('python3 processEnsekApiInternalReadingsData.py')
+    os.system('python3 processEnsekApiInternalEstimatesData.py')
+    os.system('python3 processEnsekApiTariffsWithHistory.py')
+    os.system('python3 processEnsekApiDirectDebits.py')
+    os.system('python3 processEnsekApiStatusRegistrationsData.py')
 
