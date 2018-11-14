2c73aa22738861fdff9b0608e9c6394fb0c4460e
sakthi murugan <sakthi.murugan@igloo.energy>
2018-11-14 17:34:39 +0000

    DMRE-105: processing of D18 file.

--- process_D18/process_d18.py
+++ process_D18/process_d18.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import multiprocessing
 
 sys.path.append('..')
 
@@ -6,60 +8,119 @@ from conf import config as con
 from connections import connect_db as db
 
 
-def process_d18_data():
-    try:
-        s3 = db.get_S3_Connections_resources()
-        filename = '100444.flw'
-        bucket = 'igloo-uat-bucket'
-        key = 'iglo-d18/20181012D18/' + filename
-        obj = s3.Object(bucket, key)
-        obj_str = obj.get()['Body'].read().decode('utf-8').splitlines(True)
-        # s3.meta.client.download_file(bucket, key, 'd18_001.txt')
-        # print(obj_str)
-        line_ZHV = line_GSP = line_PCL = line_BPP = line_PPC = line_SSC = line_VMR = ''
-        line_pcl_1_2 = False
-        prev_line_GSP = ''
-        f = open('processed_d18_file.txt', "w")
+class ProcessD18:
 
-        # read lines in one file
-        for lines in obj_str[:1100]:
-            if lines.split('|')[0] == 'ZHV':
-                line_ZHV = lines.replace('\n', '')
-            elif lines.split('|')[0] == 'GSP':
-                line_GSP = lines.replace('\n', '')
-            elif lines.split('|')[0] == 'PCL':
-                if int(lines.split('|')[1]) in (1, 2):
-                    line_pcl_1_2 = True
-                    line_PCL = lines.replace('\n', '')
-                else:
-                    line_pcl_1_2 = False
-            if line_pcl_1_2:
-                if lines.split('|')[0] == 'BPP':
-                    line_BPP = lines.replace('\n', '')
-                elif lines.split('|')[0] == 'SSC':
-                    line_SSC = lines.replace('\n', '')
-                elif lines.split('|')[0] == 'VMR':
-                    line_VMR = lines.replace('\n', '')
-                elif lines.split('|')[0] == 'PPC':
-                    line_PPC = lines.replace('\n', '')
+    def __init__(self):
+        self.bucket_name = 'igloo-uat-bucket'
+        self.prefix = 'D018/'
+        self.suffix = '.flw'
+        self.d018_archive_key = 'D018Archive'
+        self.upload_key_BPP = 'D018CSV/BPP/'
+        self.upload_key_PPC = 'D018CSV/PPC/'
+        self.D018_dir = sys.path[0] + '/files/D018/'
+        self.s3 = db.get_S3_Connections_client()
 
-            line_head = line_ZHV + line_GSP + line_PCL
+    def process_d18_data(self, d18_keys):
 
-            if prev_line_GSP != line_GSP and line_BPP != '':
-                prev_line_GSP = line_GSP
-                line_BPP_1 = line_head + line_BPP
-                f.write(line_BPP_1 + '\n')
-                print(line_BPP_1)
+        try:
+            # loop each file
+            for d18key in d18_keys:
+                print(d18key)
+                obj = self.s3.get_object(Bucket=self.bucket_name, Key=d18key)
 
-            if line_PPC != '':
-                line_PPC_1 = line_head + line_SSC + line_VMR + line_PPC
-                f.write(line_PPC_1 + '\n')
-                print(line_PPC_1)
-    except:
-        raise
-    finally:
-        f.close()
+                obj_str = obj['Body'].read().decode('utf-8').splitlines(True)
+
+                if not os.path.exists(self.D018_dir):
+                    os.makedirs(self.D018_dir)
+
+                filename = d18key.replace('D018/', '')
+                fileBPP_csv = filename.replace('.flw', '_BPP.csv')
+                filePPC_csv = filename.replace('.flw', '_PPC.csv')
+
+                # initializing variables
+                line_BPP_2 = line_PPC_2 = line_BPP_1 = prev_line_PPC = prev_line_BPP = line_ZHV = line_GSP = line_PCL = line_BPP = line_PPC = line_SSC = line_VMR = ''
+
+                # read lines in one file
+                for lines in obj_str:
+                    if lines.split('|')[0] == 'ZHV':
+                        line_ZHV = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'GSP':
+                        line_GSP = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'PCL':
+                        line_PCL = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'BPP':
+                        line_BPP = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'SSC':
+                        line_SSC = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'VMR':
+                        line_VMR = lines.replace('\n', '')
+                    elif lines.split('|')[0] == 'PPC':
+                        line_PPC = lines.replace('\n', '')
+
+                    if line_BPP != '' and line_BPP != prev_line_BPP:
+                        line_BPP_1 = line_ZHV + line_GSP + line_PCL + line_BPP
+                        prev_line_BPP = line_BPP
+                        # f_BPP.write(line_BPP_1 + '\n')
+                        line_BPP_2 += line_BPP_1
+                        # print(line_BPP_1)
+
+                    if line_PPC != '' and line_PPC != prev_line_PPC:
+                        line_PPC_1 = line_ZHV + line_GSP + line_PCL + line_SSC + line_VMR + line_PPC
+                        prev_line_PPC = line_PPC
+                        # f_PPC.write(line_PPC_1 + '\n')
+                        line_PPC_2 += line_PPC_1 + '\n'
+                        # print(line_PPC_1)
+
+                # upload to s3
+                self.s3.put_object(Bucket=self.bucket_name, Key=self.upload_key_BPP + fileBPP_csv, Body=line_BPP_2)
+                self.s3.put_object(Bucket=self.bucket_name, Key=self.upload_key_PPC + filePPC_csv, Body=line_PPC_2)
+
+                # archive d18
+                copy_source = {
+                                'Bucket': self.bucket_name,
+                                'Key': d18key
+                              }
+
+                self.s3.copy(copy_source, self.bucket_name, self.d018_archive_key + '/' + filename)
+
+        except:
+            raise
+        # finally:
+            # f_BPP.close()
+            # f_PPC.close()
+
+    def get_keys_from_s3(self):
+        d18_keys_s3 = []
+        # get all the files in D018 object
+        for obj in self.s3.list_objects(Bucket=self.bucket_name, Prefix=self.prefix)['Contents']:
+            if obj['Key'].endswith(self.suffix):
+                d18_keys_s3.append(obj['Key'])
+        return d18_keys_s3
 
 
 if __name__ == "__main__":
-    process_d18_data()
+
+    p = ProcessD18()
+    d18_keys_s3 = p.get_keys_from_s3()
+    k = int(len(d18_keys_s3) / 5)
+    processes = []
+    l = 0
+
+    for i in range(6):
+        print(i)
+        u = i * k
+        if i == 5:
+            # print(d18_keys[l:])
+            t = multiprocessing.Process(target=p.process_d18_data, args=(d18_keys_s3[l:],))
+        else:
+            # print(d18_keys[l:u])
+            t = multiprocessing.Process(target=p.process_d18_data, args=(d18_keys_s3[l:u],))
+        l = u
+        processes.append(t)
+
+    for p in processes:
+        p.start()
+
+    for process in processes:
+        process.join()
+
