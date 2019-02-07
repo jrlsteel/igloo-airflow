82a76e17cb69c15f25224a90bc52d051a88c5e18
Jonathan Steel <jonathan.steel@igloo.energy>
2019-02-07 11:23:11 +0000

    DMRE-161
     Conflicts:
    	common/utils.py
    	proceeLandRegistry/eng_processLandRegistry.py

---
+++ process_ensek_api/processAllEnsekPA.py
@@ -0,0 +1,37 @@
+import sys
+import subprocess
+import timeit
+from datetime import datetime
+
+
+sys.path.append('..')
+from common import utils as util
+
+
+def process_all_ensek_pa_scripts():
+
+    # Aliases are different on different OS
+    pythonAlias = util.get_pythonAlias()
+
+    print("{0}: >>>> Meter Points <<<<".format(datetime.now().strftime('%H:%M:%S')))
+    try:
+        start = timeit.default_timer()
+        subprocess.run([pythonAlias, "processEnsekApiMeterPointsData.py"])
+        print("{0}: Meter Points completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'), float(timeit.default_timer() - start)))
+    except:
+        raise
+
+    print("{0}: >>>> Internal Readings <<<<".format(datetime.now().strftime('%H:%M:%S')))
+    try:
+        start = timeit.default_timer()
+        subprocess.run([pythonAlias, "processEnsekApiInternalReadingsData.py"])
+        print("{0}: Internal Readings completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
+                                                                           float(timeit.default_timer() - start)))
+    except:
+        raise
+
+    return True
+
+
+if __name__ == '__main__':
+    response = process_all_ensek_pa_scripts()
