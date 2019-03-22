9488228218f4ae5b053af0f6029643053741adc7
Jonathan Steel <jonathan.steel@igloo.energy>
2019-03-22 10:52:45 +0000

    DMRE-340

--- process_ensek_api/processRegistrationStatus/start_ensek_registration_status_jobs.py
+++ process_ensek_api/processRegistrationStatus/start_ensek_registration_status_jobs.py
@@ -8,55 +8,36 @@ from common import process_glue_job as glue
 from common import utils as util
 
 
-class StartD18Jobs:
+class StartRegistrationsMeterpointsStatusJobs:
     def __init__(self):
         self.pythonAlias = util.get_pythonAlias()
         self.env = util.get_env()
         self.dir = util.get_dir()
 
-    def submit_download_d18_job(self):
-        """
-        Calls the d18 download_d18.py script to download the the d18 files through sftp from Ensek and push the data to s3.
-        :return: None
-        """
-
-        pythonAlias = util.get_pythonAlias()
-
-        print("{0}: >>>> Downloading D18 files <<<<".format(datetime.now().strftime('%H:%M:%S')))
+    def submit_registrations_meterpoints_status_scripts(self):
         try:
-            start = timeit.default_timer()
-            subprocess.run([pythonAlias, "download_d18.py"])
-            print("{0}: download_d18 completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
-                                                                          float(timeit.default_timer() - start)))
+            all_ensek_pa_scripts_response = ae.process_all_ensek_pa_scripts()
+            if all_ensek_pa_scripts_response:
+                print("{0}: All Ensek Scripts job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
+                # return all_ensek_scripts_response
+            else:
+                print("Error occurred in All Ensek Scripts job")
+                # return all_ensek_scripts_response
+                raise Exception
         except Exception as e:
-            print("Error in download_d18 process :- " + str(e))
+            print("Error in Ensek Scripts :- " + str(e))
             sys.exit(1)
 
-    def submit_process_d18_job(self):
-        """
-        Calls the d18 process_d18.py script to which processes the downloaded data from s3 and extracts the BPP and PPC co efficients.
-        :return: None
-        """
-
-        print("{0}: >>>> Process D18 files <<<<".format(datetime.now().strftime('%H:%M:%S')))
-        try:
-            start = timeit.default_timer()
-            subprocess.run([self.pythonAlias, "process_d18.py"])
-            print("{0}: Process D18 files completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
-                                                                               float(timeit.default_timer() - start)))
-        except Exception as e:
-            print("Error in download_d18 process :- " + str(e))
-            sys.exit(1)
-
-    def submit_d18_staging_gluejob(self):
+    def submit_registrations_meterpoints_status_staging_Gluejob(self):
         try:
             jobName = self.dir['glue_staging_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
 
-            obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='d18')
-            staging_job_response = obj_stage.run_glue_job()
-            if staging_job_response:
+            obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment,
+                                            processJob='ensek')
+            job_response = obj_stage.run_glue_job()
+            if job_response:
                 print("{0}: Staging Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -67,45 +48,44 @@ class StartD18Jobs:
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
 
-    def submit_d18_gluejob(self):
+    def submit_registrations_meterpoints_status_Gluejob(self):
         try:
-            jobName = self.dir['glue_d18_job_name']
+            jobname = self.dir['glue_customerDB_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
 
-            obj_d18 = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='d18')
-            d18_job_response = obj_d18.run_glue_job()
-            if d18_job_response:
-                print("{0}: D18 Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
+            obj_customerDB = glue.ProcessGlueJob(job_name=jobname, s3_bucket=s3_bucket, environment=environment,
+                                                 processJob='')
+            job_response = obj_customerDB.run_glue_job()
+            if job_response:
+                print("{0}: CustomerDB Glue Job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
-                print("Error occurred in D18 Job")
+                print("Error occurred in CustomerDB Glue Job")
                 # return staging_job_response
                 raise Exception
         except Exception as e:
-            print("Error in D18 Job :- " + str(e))
+            print("Error in Customer DB Job :- " + str(e))
             sys.exit(1)
 
 
 if __name__ == '__main__':
 
-    s = StartD18Jobs()
+    s = StartRegistrationsMeterpointsStatusJobs()
 
-    # run download d18 python script
-    print("{0}: download_d18 job is running...".format(datetime.now().strftime('%H:%M:%S')))
-    s.submit_download_d18_job()
+    # run Registrations Meterpoints Status
+    print("{0}:  CustomerDB Jobs running...".format(datetime.now().strftime('%H:%M:%S')))
+    s.submit_registrations_meterpoints_status_scripts()
 
-    # run processing d18 python script
-    print("{0}: process_d18 job is running...".format(datetime.now().strftime('%H:%M:%S')))
-    s.submit_process_d18_job()
+    # run PA Ensek Jobs
+    print("{0}:  PA Ensek Jobs running...".format(datetime.now().strftime('%H:%M:%S')))
+    s.submit_registrations_meterpoints_status_staging_Gluejob()
 
     # run staging glue job
     print("{0}: Staging Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    s.submit_d18_staging_gluejob()
+    s.submit_registrations_meterpoints_status_Gluejob()
 
-    # run reference d18 glue job
-    print("{0}: D18 Glue Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    s.submit_d18_gluejob()
+
 
     print("{0}: All D18 completed successfully".format(datetime.now().strftime('%H:%M:%S')))
 
