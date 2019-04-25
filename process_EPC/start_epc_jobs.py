bdc47a7249bf02052983b01da3ea76370b5c9831
Jonathan Steel <jonathan.steel@igloo.energy>
2019-04-25 11:21:25 +0100

    DMRE-420

--- process_EPC/start_epc_jobs.py
+++ process_EPC/start_epc_jobs.py
@@ -15,8 +15,11 @@ class StartEPCJobs:
         self.pythonAlias = util.get_pythonAlias()
         self.env = util.get_env()
         self.dir = util.get_dir()
-
-
+        self.certificates_jobid = util.get_jobID()
+        self.recommendations_jobid = util.get_jobID()
+        self.certificates_staging_jobid = util.get_jobID()
+        self.recommendations_staging_jobid = util.get_jobID()
+        self.certificates_ref_jobid = util.get_jobID()
 
     def submit_process_epc_certificates_job(self):
         """
@@ -26,11 +29,13 @@ class StartEPCJobs:
 
         print("{0}: >>>> Process EPC Certificates Data <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
+            util.batch_logging_insert(self.certificates_jobid, 16, 'epc_certificates_extract_pyscript', 'start_epc_jobs.py')
             start = timeit.default_timer()
             subprocess.run([self.pythonAlias, "process_external_EPC_certificates.py"])
-            print("{0}: Process EPC Certificates files completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
-                                                                               float(timeit.default_timer() - start)))
+            util.batch_logging_update(self.certificates_jobid, 'e')
+            print("{0}: Process EPC Certificates files completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),float(timeit.default_timer() - start)))
         except Exception as e:
+            util.batch_logging_update(self.certificates_jobid, 'f', str(e))
             print("Error in EPC Certificates process :- " + str(e))
             sys.exit(1)
 
@@ -42,12 +47,13 @@ class StartEPCJobs:
 
         print("{0}: >>>> Process EPC Recommendations Data <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
+            util.batch_logging_insert(self.recommendations_jobid, 17, 'epc_recommendations_extract_pyscript','start_epc_jobs.py')
             start = timeit.default_timer()
             subprocess.run([self.pythonAlias, "process_external_EPC_recommendations.py"])
-            print("{0}: Process EPC Recommendations files completed in {1:.2f} seconds".format(
-                datetime.now().strftime('%H:%M:%S'),
-                float(timeit.default_timer() - start)))
+            util.batch_logging_update(self.recommendations_jobid, 'e')
+            print("{0}: Process EPC Recommendations files completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),float(timeit.default_timer() - start)))
         except Exception as e:
+            util.batch_logging_update(self.recommendations_jobid, 'f', str(e))
             print("Error in EPC Recommendations process :- " + str(e))
             sys.exit(1)
 
@@ -58,13 +64,14 @@ class StartEPCJobs:
                 """
         print("{0}: >>>> Staging EPC Certificates Data <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
+            util.batch_logging_insert(self.certificates_staging_jobid, 18, 'epc_certificates_staging_glue_job','start_epc_jobs.py')
             jobName = self.dir['glue_staging_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
-
             obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='epc-certificates')
             staging_job_response = obj_stage.run_glue_job()
             if staging_job_response:
+                util.batch_logging_update(self.certificates_staging_jobid, 'e')
                 print("{0}: Staging Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -72,6 +79,7 @@ class StartEPCJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.certificates_staging_jobid, 'f', str(e))
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
 
@@ -82,6 +90,7 @@ class StartEPCJobs:
                       """
         print("{0}: >>>> Staging EPC Recommendations Data <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
+            util.batch_logging_insert(self.recommendations_staging_jobid, 19, 'epc_recommendations_staging_glue_job','start_epc_jobs.py')
             jobName = self.dir['glue_staging_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
@@ -89,6 +98,7 @@ class StartEPCJobs:
             obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='epc-recommendations')
             staging_job_response = obj_stage.run_glue_job()
             if staging_job_response:
+                util.batch_logging_update(self.recommendations_staging_jobid, 'e')
                 print("{0}: Staging Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -96,6 +106,7 @@ class StartEPCJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.recommendations_staging_jobid, 'f', str(e))
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
 
@@ -105,10 +116,11 @@ class StartEPCJobs:
             jobName = self.dir['glue_epc_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
-
+            util.batch_logging_insert(self.certificates_ref_jobid, 20, 'epc_certificates_ref_glue_job','start_epc_jobs.py')
             obj = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='epc_certificates')
             job_response = obj.run_glue_job()
             if job_response:
+                util.batch_logging_update(self.certificates_ref_jobid, 'e')
                 print("{0}: Ref Glue Job Completed successfully for {1}".format(datetime.now().strftime('%H:%M:%S'), self.process_epc_cert_name))
                 # return staging_job_response
             else:
@@ -116,6 +128,7 @@ class StartEPCJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.certificates_ref_jobid, 'f', str(e))
             print("Error in Ref Glue Job :- " + str(e))
             sys.exit(1)
 
