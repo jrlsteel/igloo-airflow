3828d447ddd2dde0000ed7cfdc8e9cf31d8f7a8f
Jonathan Steel <jonathan.steel@igloo.energy>
2019-04-25 10:58:26 +0100

    DMRE-420

--- process_Ensek/start_ensek_api_jobs.py
+++ process_Ensek/start_ensek_api_jobs.py
@@ -16,20 +16,10 @@ class StartEnsekJobs:
     def __init__(self):
         self.env = util.get_env()
         self.dir = util.get_dir()
+        self.staging_jobid = util.get_jobID()
+        self.ref_jobid = util.get_jobID()
 
-    def submit_schema_validations(self):
-        try:
-            schema_validation_response = vs.processAccounts()
-            if schema_validation_response:
-                print("{0}: Schema Validation job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
-                # return schema_validation_response
-            else:
-                print("Error occurred in Schema Validation job")
-                # return schema_validation_response
-                raise Exception
-        except Exception as e:
-            print("Error in Schema Validation :- " + str(e))
-            sys.exit(1)
+
 
     def submit_all_ensek_scripts(self):
         try:
@@ -51,9 +41,11 @@ class StartEnsekJobs:
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
 
+            util.batch_logging_insert(self.staging_jobid, 13, 'ensek_non_pa_staging_gluejob', 'start_ensek_api_jobs.py')
             obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='ensek')
             job_response = obj_stage.run_glue_job()
             if job_response:
+                util.batch_logging_update(self.staging_jobid, 'e')
                 print("{0}: Staging Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -61,38 +53,22 @@ class StartEnsekJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.staging_jobid, 'f', str(e))
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
 
-    def submit_customerDB_Gluejob(self):
-        try:
-            jobname = self.dir['glue_customerDB_job_name']
-            s3_bucket = self.dir['s3_bucket']
-            environment = self.env
-
-            obj_customerDB = glue.ProcessGlueJob(job_name=jobname, s3_bucket=s3_bucket, environment=environment, processJob='')
-            job_response = obj_customerDB.run_glue_job()
-            if job_response:
-                print("{0}: CustomerDB Glue Job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
-                # return staging_job_response
-            else:
-                print("Error occurred in CustomerDB Glue Job")
-                # return staging_job_response
-                raise Exception
-        except Exception as e:
-            print("Error in Customer DB Job :- " + str(e))
-            sys.exit(1)
 
     def submit_Ensek_Gluejob(self):
         try:
             jobname = self.dir['glue_ensek_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
-
+            util.batch_logging_insert(self.ref_jobid, 14, 'ensek_non_pa_ref_gluejob','start_ensek_api_jobs.py')
             obj_ensek = glue.ProcessGlueJob(job_name=jobname, s3_bucket=s3_bucket, environment=environment,
                                             processJob='ensek')
             job_response = obj_ensek.run_glue_job()
             if job_response:
+                util.batch_logging_update(self.ref_jobid, 'e')
                 print("{0}: Ensek Glue Job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -100,45 +76,9 @@ class StartEnsekJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.ref_jobid, 'f', str(e))
             print("Error in Ensek Job :- " + str(e))
             sys.exit(1)
-
-    def submit_eac_aq_gluejob(self):
-        try:
-            jobName = self.dir['glue_eac_aq_job_name']
-            s3_bucket = self.dir['s3_bucket']
-            environment = self.env
-
-            obj_d18 = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment,
-                                          processJob='eac_aq')
-            d18_job_response = obj_d18.run_glue_job()
-            if d18_job_response:
-                print("{0}: EAC and AQ Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
-                # return staging_job_response
-            else:
-                print("Error occurred in EAC and AQ Job")
-                # return staging_job_response
-                raise Exception
-        except Exception as e:
-            print("Error in EAC and AQ Job :- " + str(e))
-            sys.exit(1)
-
-
-
-
-# def submit_ensek_counts(self):
-#      try:
-#          ensek_counts_response = ec.process_count()
-#          if ensek_counts_response:
-#              print("{0}: Ensek Counts Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
-#              # return ensek_counts_response
-#          else:
-#              print("Error occurred in Ensek Count Job")
-#              # return ensek_counts_response
-#              raise Exception
-#      except Exception as e:
-#          print("Error in Ensek Counts job :- " + str(e))
-#          sys.exit(1)
 
 
 if __name__ == '__main__':
