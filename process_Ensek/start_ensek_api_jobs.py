927ec3bbdd02403218a8acddafe133750b9e7b0d
Jonathan Steel <jonathan.steel@igloo.energy>
2019-04-25 10:59:25 +0100

    DMRE-420

--- process_Ensek/start_ensek_api_jobs.py
+++ process_Ensek/start_ensek_api_jobs.py
@@ -3,9 +3,7 @@ from datetime import datetime
 
 sys.path.append('..')
 
-from process_Ensek.schema_validation import validateSchema as vs
 from process_Ensek import processAllEnsekScripts as ae
-# from process_Ensek import processEnsekApiCounts as ec
 from common import process_glue_job as glue
 
 from common import utils as util
