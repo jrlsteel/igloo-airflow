282d0c14f5e6913debf229439d9c0b3af529fe7d
Jonathan Steel <jonathan.steel@igloo.energy>
2019-05-29 11:18:26 +0100

    dmre-492

--- process_Nosi/start_ensek_readings_nosi_jobs.py
+++ process_Nosi/start_ensek_readings_nosi_jobs.py
@@ -14,9 +14,10 @@ class StartReadingsNOSIJobs:
         self.env = util.get_env()
         self.dir = util.get_dir()
         self.all_jobid = util.get_jobID()
-        self.staging_jobid = util.get_jobID()
-        self.ref_jobid = util.get_jobID()
-
+        self.nosi_jobid = util.get_jobID()
+        self.nosi_download_jobid = util.get_jobID()
+        self.nosi_staging_jobid = util.get_jobID()
+        self.nosi_ref_jobid = util.get_jobID()
 
     # def submit_readings_internal_job(self):
     #     """
@@ -37,6 +38,7 @@ class StartReadingsNOSIJobs:
     def submit_internal_readings_nosi_staging_gluejob(self):
         try:
             jobName = self.dir['glue_staging_job_name']
+            util.batch_logging_insert(self.nosi_staging_jobid, 43, 'nosi_staging_glue_job', 'start_ensek_readings_nosi_jobs.py')
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
 
@@ -44,6 +46,7 @@ class StartReadingsNOSIJobs:
                                             processJob='ensek-readings-nosi')
             job_response = obj_stage.run_glue_job()
             if job_response:
+                util.batch_logging_update(self.nosi_staging_jobid, 'e')
                 print("{0}: Staging Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # return staging_job_response
             else:
@@ -51,12 +54,15 @@ class StartReadingsNOSIJobs:
                 # return staging_job_response
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.nosi_staging_jobid, 'f', str(e))
+            util.batch_logging_update(self.all_jobid, 'f', str(e))
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
 
     def submit_internal_readings_nosi_gluejob(self):
         try:
             jobname = self.dir['glue_internal_readings_nosi_job_name']
+            util.batch_logging_insert(self.nosi_ref_jobid, 44, 'nosi_ref_glue_job', 'start_ensek_readings_nosi_jobs.py')
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
 
@@ -64,6 +70,7 @@ class StartReadingsNOSIJobs:
                                                  processJob='ensek_ref_readings_nosi')
             job_response = obj_submit_registrations_meterpoints_status_Gluejob.run_glue_job()
             if job_response:
+                util.batch_logging_update(self.nosi_ref_jobid, 'e')
                 print("{0}: Ensek Internal Readings NOSI Glue Job completed successfully".format(datetime.now().strftime('%H:%M:%S')))
                 # returnsubmit_internal_readings_Gluejob
             else:
@@ -71,6 +78,8 @@ class StartReadingsNOSIJobs:
                 # return submit_internal_readings_nosi_Gluejob
                 raise Exception
         except Exception as e:
+            util.batch_logging_update(self.nosi_ref_jobid, 'f', str(e))
+            util.batch_logging_update(self.all_jobid, 'f', str(e))
             print("Error in Internal Readings NOSI  DB Job :- " + str(e))
             sys.exit(1)
 
