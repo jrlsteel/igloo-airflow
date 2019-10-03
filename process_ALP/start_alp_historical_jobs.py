39773e23f23cb503a64a3fedfb3cc1dbd13b4376
BenJesty <ben.jesty@igloo.energy>
2019-10-03 12:26:48 +0100

    DMRE-854 - Commented out sections from old and new files to call calculated steps

--- process_ALP/start_alp_historical_jobs.py
+++ process_ALP/start_alp_historical_jobs.py
@@ -5,12 +5,12 @@ import subprocess
 
 sys.path.append('..')
 
-from process_eac_aq import start_eac_aq_pa_jobs as eacaqpa
-from process_eac_aq import start_igloo_ind_eac_aq_jobs as iglindeacaq
-from process_eac_aq import start_consumption_accuracy_jobs as ca
-from process_tado import start_tado_efficiency_jobs as ta
-from process_EstimatedAdvance import start_est_advance_job as est_adv
-from process_aurora import start_daily_sales_jobs as ds
+# from process_eac_aq import start_eac_aq_pa_jobs as eacaqpa
+# from process_eac_aq import start_igloo_ind_eac_aq_jobs as iglindeacaq
+# from process_eac_aq import start_consumption_accuracy_jobs as ca
+# from process_tado import start_tado_efficiency_jobs as ta
+# from process_EstimatedAdvance import start_est_advance_job as est_adv
+# from process_aurora import start_daily_sales_jobs as ds
 
 from common import process_glue_job as glue
 from common import utils as util
@@ -32,9 +32,9 @@ class ALP:
         self.alp_cv_staging_jobid = util.get_jobID()
         self.alp_ref_jobid = util.get_jobID()
 
-        self.eac_aq_pa_ref_jobid = util.get_jobID()
-        self.eac_aq_v1_ref_jobid = util.get_jobID()
-        self.eac_aq_cons_accu_ref_jobid = util.get_jobID()
+        # self.eac_aq_pa_ref_jobid = util.get_jobID()
+        # self.eac_aq_v1_ref_jobid = util.get_jobID()
+        # self.eac_aq_cons_accu_ref_jobid = util.get_jobID()
 
     def submit_process_alp_wcf_job(self):
         """
@@ -216,35 +216,35 @@ if __name__ == '__main__':
     print("{0}: ALP Glue Job running...".format(datetime.now().strftime('%H:%M:%S')))
     s.submit_alp_gluejob()
 
-    # run eac and aq pa calculation job
-    print("{0}: EAC and AQ Glue Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    eacaqpa_obj = eacaqpa.EacAqPa()
-    eacaqpa_obj.submit_eac_aq_gluejob()
-
-    # run eac and aq v1 calculation job
-    print("{0}: EAC and AQ Glue V1 Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    iglindeacaq_obj = iglindeacaq.IglIndEacAq()
-    iglindeacaq_obj.submit_eac_aq_gluejob()
+    # # run eac and aq pa calculation job
+    # print("{0}: EAC and AQ Glue Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # eacaqpa_obj = eacaqpa.EacAqPa()
+    # eacaqpa_obj.submit_eac_aq_gluejob()
     #
-    # # run consumption accuracy job
-    print("{0}: Consumption Accuracy Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    ca_obj = ca.ConsumptionAccuracy()
-    ca_obj.submit_consumption_accuracy_gluejob()
-
-    # run TADO efficiency job
-    print("{0}: TADO Efficiency Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    ta_obj = ta.TADOEfficiencyJobs()
-    ta_obj.submit_tado_efficiency_batch_gluejob()
-
-    # run daily sales job
-    print("{0}: Daily Sales Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    ds_obj = ds.DailySalesJobs()
-    ds_obj.submit_daily_sales_batch_gluejob()
-
-    # run Estimated Advance Job
-    print("{0}: Estimated Advance Job running...".format(datetime.now().strftime('%H:%M:%S')))
-    est_adv_obj = est_adv.EstimatedAdvance()
-    est_adv_obj.submit_estimated_advance_gluejob()
+    # # run eac and aq v1 calculation job
+    # print("{0}: EAC and AQ Glue V1 Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # iglindeacaq_obj = iglindeacaq.IglIndEacAq()
+    # iglindeacaq_obj.submit_eac_aq_gluejob()
+    # #
+    # # # run consumption accuracy job
+    # print("{0}: Consumption Accuracy Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # ca_obj = ca.ConsumptionAccuracy()
+    # ca_obj.submit_consumption_accuracy_gluejob()
+    #
+    # # run TADO efficiency job
+    # print("{0}: TADO Efficiency Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # ta_obj = ta.TADOEfficiencyJobs()
+    # ta_obj.submit_tado_efficiency_batch_gluejob()
+    #
+    # # run daily sales job
+    # print("{0}: Daily Sales Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # ds_obj = ds.DailySalesJobs()
+    # ds_obj.submit_daily_sales_batch_gluejob()
+    #
+    # # run Estimated Advance Job
+    # print("{0}: Estimated Advance Job running...".format(datetime.now().strftime('%H:%M:%S')))
+    # est_adv_obj = est_adv.EstimatedAdvance()
+    # est_adv_obj.submit_estimated_advance_gluejob()
 
     print("{0}: All {1} completed successfully".format(datetime.now().strftime('%H:%M:%S'), s.process_name))
 
