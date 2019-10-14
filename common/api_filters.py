23e7d82283acd905d06fabb4d6bd2e8e74a9280b
BenJesty <ben.jesty@igloo.energy>
2019-10-14 17:11:28 +0100

    DMRE-861 - updated api_filters.py sql to include reference to new occupier accounts table

--- common/api_filters.py
+++ common/api_filters.py
@@ -1,16 +1,24 @@
 account_ids = {
-    "daily": """select account_id, case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
-              from (select account_id, least(supplyenddate, associationenddate) as mp_sed
-                    from ref_meterpoints_raw mpr) mps
-              group by account_id
-              having sed is null or datediff(weeks, sed, getdate()) < 8
-              order by account_id""",
-    "weekly": """select account_id, case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
-              from (select account_id, least(supplyenddate, associationenddate) as mp_sed
-                    from ref_meterpoints_raw mpr) mps
-              group by account_id
-              having sed is null or datediff(years, sed, getdate()) < 1
-              order by account_id""",
+    "daily": """select coalesce(sc.external_id, oa.account_id, mr.account_id)            as acc_id,
+                       case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
+                from ref_cdb_supply_contracts sc
+                         full join ref_occupier_accounts oa on sc.external_id = oa.account_id
+                         full join (select account_id, least(supplyenddate, associationenddate) as mp_sed
+                                    from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
+                group by acc_id
+                having acc_id is not null
+                   and (sed is null or datediff(weeks, sed, getdate()) < 8)
+                order by acc_id""",
+    "weekly": """select coalesce(sc.external_id, oa.account_id, mr.account_id)            as acc_id,
+                       case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
+                from ref_cdb_supply_contracts sc
+                         full join ref_occupier_accounts oa on sc.external_id = oa.account_id
+                         full join (select account_id, least(supplyenddate, associationenddate) as mp_sed
+                                    from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
+                group by acc_id
+                having acc_id is not null
+                   and (sed is null or datediff(years, sed, getdate()) < 1)
+                order by acc_id""",
 
 }
 
