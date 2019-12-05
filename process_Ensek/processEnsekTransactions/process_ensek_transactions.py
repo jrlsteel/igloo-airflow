e4d06bbb29da7adfa7aa4af57fa59c0674d90694
BenJesty <ben.jesty@igloo.energy>
2019-12-05 12:25:43 +0000

    Added logging for API call time

--- process_Ensek/processEnsekTransactions/process_ensek_transactions.py
+++ process_Ensek/processEnsekTransactions/process_ensek_transactions.py
@@ -25,12 +25,17 @@ class AccountTransactions:
     max_calls = con.api_config['max_api_calls']
     rate = con.api_config['allowed_period_in_secs']
 
-    def __init__(self):
-        pass
+    def __init__(self, process_num):
+        logs_dir_path = sys.path[0] + '/logs/'
+        if not os.path.exists(logs_dir_path):
+            os.makedirs(logs_dir_path)
+        self.pnum = process_num
+        self.generic_log_file = sys.path[0] + '/logs/' + 'account_transactions_generic_logs_' + time.strftime(
+            '%Y%m%d') + '.csv'
 
     @sleep_and_retry
     @limits(calls=max_calls, period=rate)
-    def get_api_response(self, api_url, head):
+    def get_api_response(self, api_url, head, account_id):
         '''
              get the response for the respective url that is passed as part of this function
         '''
@@ -38,21 +43,33 @@ class AccountTransactions:
         timeout = con.api_config['connection_timeout']
         retry_in_secs = con.api_config['retry_in_secs']
         i = 0
+        attempt_num = 0
+        total_api_time = 0.0
+        api_call_start = 0.0
         while True:
             try:
+                attempt_num += 1
+                api_call_start = time.time()
                 response = requests.get(api_url, headers=head)
+                total_api_time += time.time() - api_call_start
                 if response.status_code == 200:
+                    method_time = time.time() - start_time
+                    self.log_msg('{0}, {1}, {2}, {3}, {4}'.format(account_id, 'success', total_api_time, attempt_num,
+                                                                  method_time),
+                                 msg_type='timing')
                     return json.loads(response.content.decode('utf-8'))
                 else:
                     print('Problem Grabbing Data: ', response.status_code)
+                    exit_type = 'format_error'
                     self.log_error('Response Error: Problem grabbing data', response.status_code)
                     break
 
             except ConnectionError:
+                total_api_time += time.time() - api_call_start
                 if time.time() > start_time + timeout:
                     print('Unable to Connect after {} seconds of ConnectionErrors'.format(timeout))
                     self.log_error('Unable to Connect after {} seconds of ConnectionErrors'.format(timeout))
-
+                    exit_type = 'connection_error'
                     break
                 else:
                     print('Retrying connection in ' + str(retry_in_secs) + ' seconds' + str(i))
@@ -60,9 +77,11 @@ class AccountTransactions:
 
                     time.sleep(retry_in_secs)
             i = i + retry_in_secs
+        method_time = time.time() - start_time
+        self.log_msg('{0}, {1}, {2}, {3}, {4}'.format(account_id, exit_type, total_api_time, attempt_num, method_time),
+                     msg_type='timing')
 
     def extract_account_transactions_json(self, data, account_id, k, dir_s3):
-
         df_account_transactions = json_normalize(data)
         df_account_transactions.columns = df_account_transactions.columns.str.replace('.', '_')
         df_account_transactions['account_id'] = account_id
@@ -83,16 +102,21 @@ class AccountTransactions:
         data_json = json.loads(data_str)
         return data_json
 
+    def log_msg(self, msg, msg_type='diagnostic'):
+        with open(self.generic_log_file, mode='a+') as log_msg_file:
+            log_msg_file.write(
+                '{0}, {1}, {2}, {3}\n'.format(self.pnum, datetime.datetime.now().strftime('%H:%M:%S'), msg, msg_type))
+
     def log_error(self, error_msg, error_code=''):
         logs_dir_path = sys.path[0] + '/logs/'
         if not os.path.exists(logs_dir_path):
             os.makedirs(logs_dir_path)
-        with open(sys.path[0] + '/logs/' + 'account_transactions_logs_' + time.strftime('%d%m%Y') + '.csv', mode='a') as errorlog:
+        with open(sys.path[0] + '/logs/' + 'account_transactions_logs_' + time.strftime('%d%m%Y') + '.csv',
+                  mode='a') as errorlog:
             employee_writer = csv.writer(errorlog, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             employee_writer.writerow([error_msg, error_code])
 
     def processAccounts(self, account_ids, k, dir_s3):
-
         api_url_at, head_lb = util.get_ensek_api_info1('account_transactions')
 
         for account_id in account_ids:
@@ -100,7 +124,7 @@ class AccountTransactions:
 
             # Get Accounts Transactions
             api_url_at1 = api_url_at.format(account_id)
-            api_response_at = self.get_api_response(api_url_at1, head_lb)
+            api_response_at = self.get_api_response(api_url_at1, head_lb, account_id)
 
             if api_response_at:
                 formatted_reponse_at = self.format_json_response(api_response_at)
@@ -119,7 +143,6 @@ if __name__ == "__main__":
 
     s3 = s3_con(bucket_name)
 
-
     account_ids = []
     '''Enable this to test for 1 account id'''
     if con.test_config['enable_manual'] == 'Y':
@@ -134,13 +157,15 @@ if __name__ == "__main__":
     if con.test_config['enable_db_max'] == 'Y':
         account_ids = util.get_accountID_fromDB(True)
 
+    account_ids = account_ids[:10]
+
     # Enable to test without multiprocessing.
     # p = AccountTransactions()
     # p.processAccounts(account_ids, s3, dir_s3)
 
     ####### Multiprocessing Starts #########
     env = util.get_env()
-    total_processes = util.get_multiprocess('total_ensek_processes')
+    total_processes = 2  # util.get_multiprocess('total_ensek_processes')
 
     if env == 'uat':
         n = total_processes  # number of process to run in parallel
@@ -157,7 +182,7 @@ if __name__ == "__main__":
     start = timeit.default_timer()
 
     for i in range(n + 1):
-        p1 = AccountTransactions()
+        p1 = AccountTransactions(i)
         print(i)
         uv = i * k
         if i == n:
@@ -179,7 +204,3 @@ if __name__ == "__main__":
     ###### Multiprocessing Ends #########
 
     print("Process completed in " + str(timeit.default_timer() - start) + ' seconds')
-
-
-
-
