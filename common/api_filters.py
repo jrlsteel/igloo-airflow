5c1b8d4b5c1d10659c6b6cdae48e284338ae2333
BenJesty <ben.jesty@igloo.energy>
2020-01-27 17:33:57 +0000

    DMRE-1025 - changed api filtering sql to look at all live accounts rather than pending live

--- common/api_filters.py
+++ common/api_filters.py
@@ -30,12 +30,35 @@ acc_mp_ids = {
 }
 
 pending_acc_ids = {
-    "daily": """select account_id, min(greatest(associationstartdate, supplystartdate)) as ssd
-from ref_meterpoints
-group by account_id
-having datediff(days, ssd, getdate()) <= 7""",
-    "weekly": """select account_id, min(greatest(associationstartdate, supplystartdate)) as ssd
-from ref_meterpoints
-group by account_id
-having datediff(days, ssd, getdate()) <= 7"""
+    "daily": """select coalesce(sc.external_id, oa.account_id, mr.account_id)            as acc_id,
+                       case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
+                from ref_cdb_supply_contracts sc
+                         full join ref_occupier_accounts oa on sc.external_id = oa.account_id
+                         full join (select account_id, least(supplyenddate, associationenddate) as mp_sed
+                                    from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
+                group by acc_id
+                having acc_id is not null
+                   and (sed is null or datediff(years, sed, getdate()) < 1)
+                order by acc_id""",
+    "weekly": """select coalesce(sc.external_id, oa.account_id, mr.account_id)            as acc_id,
+                       case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
+                from ref_cdb_supply_contracts sc
+                         full join ref_occupier_accounts oa on sc.external_id = oa.account_id
+                         full join (select account_id, least(supplyenddate, associationenddate) as mp_sed
+                                    from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
+                group by acc_id
+                having acc_id is not null
+                   and (sed is null or datediff(years, sed, getdate()) < 1)
+                order by acc_id"""
 }
+
+# pending_acc_ids = {
+#     "daily": """select account_id, min(greatest(associationstartdate, supplystartdate)) as ssd
+# from ref_meterpoints
+# group by account_id
+# having datediff(days, ssd, getdate()) <= 7""",
+#     "weekly": """select account_id, min(greatest(associationstartdate, supplystartdate)) as ssd
+# from ref_meterpoints
+# group by account_id
+# having datediff(days, ssd, getdate()) <= 7"""
+# }
