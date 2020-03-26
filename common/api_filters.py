68120ded2e26400d24a52a7fc8203104c468e679
BenJesty <ben.jesty@igloo.energy>
2020-03-26 15:06:03 +0000

    DMRE-1159 - Updated date ranges in the API filtering SQL to be in terms of days rather than weeks & years

--- common/api_filters.py
+++ common/api_filters.py
@@ -7,7 +7,7 @@ account_ids = {
                                     from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
                 group by acc_id
                 having acc_id is not null
-                   and (sed is null or datediff(weeks, sed, getdate()) < 8)
+                   and (sed is null or datediff(days, sed, getdate()) < 56)
                 order by acc_id""",
     "weekly": """select coalesce(sc.external_id, oa.account_id, mr.account_id)            as acc_id,
                        case when count(mp_sed) < count(*) then null else max(mp_sed) end as sed
@@ -17,7 +17,7 @@ account_ids = {
                                     from ref_meterpoints_raw) mr on mr.account_id = coalesce(oa.account_id, sc.external_id)
                 group by acc_id
                 having acc_id is not null
-                   and (sed is null or datediff(years, sed, getdate()) < 1)
+                   and (sed is null or datediff(days, sed, getdate()) < 365)
                 order by acc_id""",
 
 }
@@ -25,7 +25,7 @@ account_ids = {
 acc_mp_ids = {
     "daily": """select distinct account_id, meter_point_id, meterpointnumber, meterpointtype
                 from ref_meterpoints_raw
-                where datediff(weeks, greatest(supplystartdate, associationstartdate), getdate()) < 1
+                where datediff(days, greatest(supplystartdate, associationstartdate), getdate()) < 7
                 order by account_id""",
 }
 
