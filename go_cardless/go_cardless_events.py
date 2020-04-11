a4eba124497ae277142bff9973dca156177a8126
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-04-11 16:30:15 +0100

    DMRE-1184

--- go_cardless/go_cardless_events.py
+++ go_cardless/go_cardless_events.py
@@ -1,67 +1,60 @@
+import os
+import gocardless_pro
 import timeit
 
-import boto3
-import glob
 import pandas as pd
 import numpy as np
 import requests
 import json
-from pandas.io.json import json_normalize
-from ratelimit import limits, sleep_and_retry
-import time
-from requests import ConnectionError
-import csv
-import multiprocessing
-from time import sleep
 from multiprocessing import freeze_support
 from datetime import datetime, date, time, timedelta
 import math
 
-import os
-import gocardless_pro
 from queue import Queue
-import queue
-import requests
+from pandas.io.json import json_normalize
 
 import sys
-import os
 
 sys.path.append('..')
 
 from common import utils as util
 from conf import config as con
-from connections.connect_db import get_boto_S3_Connections as s3_con
+from connections.connect_db import get_finance_S3_Connections as s3_con
 from connections import connect_db as db
+
+client = gocardless_pro.Client(access_token='live_yk11i3mKMHN454DYUjIR0Mw6y34ePfW1GJoEXHaS', environment='live')
+Events = client.events
 
 
 class GoCardlessEvents(object):
 
-    def __init__(self, execDate = datetime.now(), noDays=1 ):
+    def __init__(self, execStartDate, execEndDate):
         self.env = util.get_env()
         self.dir = util.get_dir()
         self.bucket_name = self.dir['s3_finance_bucket']
         self.s3 = s3_con(self.bucket_name)
-        self.now = datetime.now()
-        self.execDate = datetime.strptime(execDate, '%Y-%m-%d')
-        self.qtr = math.ceil(self.execDate.month/3.)
-        self.yr = math.ceil(self.execDate.year)
-        self.s3key = 'timestamp=' + str(self.yr) + '-Q'+ str(self.qtr)
-        self.filename = 'go_cardless_events_' + '{:%Y-%m-%d}'.format(self.execDate) + '.csv'
-        self.noDays = noDays
         self.fileDirectory = self.dir['s3_finance_goCardless_key']['Events']
+        self.Events = Events
+        self.execStartDate = datetime.strptime(execStartDate, '%Y-%m-%d')
+        self.execEndDate = datetime.strptime(execEndDate, '%Y-%m-%d')
+        self.qtr = math.ceil(self.execStartDate.month / 3.)
+        self.yr = math.ceil(self.execStartDate.year)
+        self.s3key = 'timestamp=' + str(self.yr) + '-Q' + str(self.qtr)
+        self.filename = 'go_cardless_events_' + '{:%Y%m}'.format(self.execStartDate) + '_' + '{:%Y%m}'.format(
+            self.execEndDate) + '.csv'
+
+    def is_json(self, myjson):
+        try:
+            json_object = json.loads(myjson)
+        except ValueError as e:
+            return False
+        return True
 
 
-    def is_json(myjson):
-      try:
-        json_object = json.loads(myjson)
-      except ValueError as e:
-        return False
-      return True
-
-
-    def get_date(self, dateFormat="%Y-%m-%d"):
-        dateStart = self.execDate
-        addDays = self.noDays
+    def get_date(self, _date, dateFormat="%Y-%m-%d"):
+        dateStart = _date
+        dateStart = datetime.strptime(dateStart, '%Y-%m-%d')
+        addDays = 1  ###self.noDays
         if (addDays != 0):
             dateEnd = dateStart + timedelta(days=addDays)
         else:
@@ -69,133 +62,122 @@ class GoCardlessEvents(object):
 
         return dateEnd.strftime(dateFormat)
 
+    def daterange(self, dateFormat="%Y-%m-%d"):
+        start_date = self.execStartDate
+        end_date = self.execEndDate
+        for n in range(int((end_date - start_date).days)):
+            seq_date = start_date + timedelta(n)
+            yield seq_date.strftime(dateFormat)
+        return seq_date
 
     def process_Events(self):
-        bucket_name = self.bucket_name
-        execStartDate = '{:%Y-%m-%d}'.format(self.execDate) + "T00:00:00.000Z"
-        execEndDate = self.get_date() + "T00:00:00.000Z"
-        s3 = self.s3
-        dir_s3 = self.dir
         fileDirectory = self.fileDirectory
-
-        client = gocardless_pro.Client(access_token= con.go_cardless['access_token'], environment= con.go_cardless['environment'])
-        # Loop through a page of payments, printing each payment's amount
+        s3 = self.s3
+        Events = self.Events
+        # Loop through a page
         q = Queue()
+        df_out = pd.DataFrame()
         datalist = []
+        ls = []
+        StartDate = '{:%Y-%m-%d}'.format(self.execStartDate) + "T00:00:00.000Z"
+        EndDate = '{:%Y-%m-%d}'.format(self.execEndDate) + "T00:00:00.000Z"
+        try:
+            for event in Events.all(
+                    params={"created_at[gte]": StartDate, "created_at[lte]": EndDate}):
+                test = []
+                id = None
 
-        # Fetch a payment by its ID
-        events = client.events
+                mandate = None
+                new_customer_bank_account = None
+                new_mandate = None
+                organisation = None
+                parent_event = None
+                payment = None
+                payout = None
+                previous_customer_bank_account = None
+                refund = None
+                subscription = None
 
-        # Loop through a page of payments, printing each payment's amount
-        df = pd.DataFrame()
-        print('.....listing events')
-        for event in events.all(params={"created_at[gt]": execStartDate, "created_at[lt]": execEndDate}):
-            try:
-                EnsekAccountId = ''
-                StatementId = ''
-                mandate = ''
-                new_customer_bank_account = ''
-                new_mandate = ''
-                organisation = ''
-                parent_event = ''
-                payment = ''
-                payout = ''
-                previous_customer_bank_account = ''
-                refund  = ''
-                subscription = ''
+                created_at = None
+                resource_type = None
+                action = None
+                customer_notifications = None
+                cause = None
+                description = None
+                origin = None
+                reason_code = None
+                scheme = None
+                will_attempt_retry = None
 
-                id = ''
-                created_at = ''
-                resource_type = ''
-                action = ''
-                customer_notifications = ''
-                cause = ''
-                description = ''
-                origin = ''
-                reason_code = ''
-                scheme = ''
-                will_attempt_retry = ''
-                if len(event.id) != 0:
-                    ## print(event.id)
-                    '''
-                    if event.metadata:
-                        if 'AccountId' in event.metadata:
-                            EnsekAccountId = event.metadata['AccountId']
-                        if 'StatementId' in event.metadata:
-                            StatementId = event.metadata['StatementId']
-                    '''
-                    if event.links.mandate and len(event.links.mandate) != 0:
-                        mandate = event.links.mandate
-                    if event.links.new_customer_bank_account and len(event.links.new_customer_bank_account) != 0:
-                        new_customer_bank_account = event.links.new_customer_bank_account
-                    if event.links.new_mandate and len(event.links.new_mandate) != 0:
-                        new_mandate = event.links.new_mandate
-                    if event.links.organisation and len(event.links.organisation) != 0:
-                        organisation = event.links.organisation
-                    if event.links.parent_event and len(event.links.parent_event) != 0:
-                        parent_event = event.links.parent_event
-                    if event.links.payment and len(event.links.payment) != 0:
-                        payment = event.links.payment
-                    if event.links.payout and len(event.links.payout) != 0:
-                        payout = event.links.payout
-                    if event.links.previous_customer_bank_account and len(event.links.previous_customer_bank_account) != 0:
-                        previous_customer_bank_account = event.links.previous_customer_bank_account
-                    if event.links.refund and len(event.links.refund) != 0:
-                        refund = event.links.refund
-                    if event.links.subscription and len(event.links.subscription) != 0:
-                        subscription = event.links.subscription
+                if self.is_json(event.id):
+                        id_js = json.loads(event.id.decode("utf-8"))
+                elif len(event.id) > 0:
+                    id = event.id
+                created_at = event.created_at
 
-                    if event.id and len(event.id) != 0:
-                        id = event.id
-                    if event.created_at and len(event.created_at) != 0:
-                        created_at = event.created_at
-                    if event.resource_type and len(event.resource_type) != 0:
-                        resource_type = event.resource_type
-                    if event.action and len(event.action) != 0:
-                        action = event.action
-                    if event.customer_notifications and len(event.customer_notifications) != 0:
-                        customer_notifications = event.customer_notifications
-                    if event.details.cause and len(event.details.cause) != 0:
-                        cause = event.details.cause
-                    if event.details.description and len(event.details.description) != 0:
-                        description = event.details.description
-                    if event.details.origin and len(event.details.origin) != 0:
-                        origin = event.details.origin
-                    if event.details.reason_code and len(event.details.reason_code) != 0:
-                        reason_code = event.details.reason_code
-                    if event.details.scheme and len(event.details.scheme) != 0:
-                        scheme = event.details.scheme
-                    if event.details.will_attempt_retry and len(event.details.will_attempt_retry) != 0:
-                        will_attempt_retry = event.details.will_attempt_retry
+                if event.links.mandate and len(event.links.mandate) != 0:
+                    mandate = event.links.mandate
+                if event.links.new_customer_bank_account and len(event.links.new_customer_bank_account) != 0:
+                    new_customer_bank_account = event.links.new_customer_bank_account
+                if event.links.new_mandate and len(event.links.new_mandate) != 0:
+                    new_mandate = event.links.new_mandate
+                if event.links.organisation and len(event.links.organisation) != 0:
+                    organisation = event.links.organisation
+                if event.links.parent_event and len(event.links.parent_event) != 0:
+                    parent_event = event.links.parent_event
+                if event.links.payment and len(event.links.payment) != 0:
+                    payment = event.links.payment
+                if event.links.payout and len(event.links.payout) != 0:
+                    payout = event.links.payout
+                if event.links.previous_customer_bank_account and len(event.links.previous_customer_bank_account) != 0:
+                    previous_customer_bank_account = event.links.previous_customer_bank_account
+                if event.links.refund and len(event.links.refund) != 0:
+                    refund = event.links.refund
+                if event.links.subscription and len(event.links.subscription) != 0:
+                    subscription = event.links.subscription
 
-                    EnsekID = EnsekAccountId
-                    EnsekStatementId = StatementId
-                    ##print(event.id)
-                    listRow = [id, created_at, resource_type, action, customer_notifications, cause, description, origin,
+                if event.resource_type and len(event.resource_type) != 0:
+                    resource_type = event.resource_type
+                if event.action and len(event.action) != 0:
+                    action = event.action
+                if event.customer_notifications and len(event.customer_notifications) != 0:
+                    customer_notifications = event.customer_notifications
+                if event.details.cause and len(event.details.cause) != 0:
+                    cause = event.details.cause
+                if event.details.description and len(event.details.description) != 0:
+                    description = event.details.description
+                if event.details.origin and len(event.details.origin) != 0:
+                    origin = event.details.origin
+                if event.details.reason_code and len(event.details.reason_code) != 0:
+                    reason_code = event.details.reason_code
+                if event.details.scheme and len(event.details.scheme) != 0:
+                    scheme = event.details.scheme
+                if event.details.will_attempt_retry and len(event.details.will_attempt_retry) != 0:
+                    will_attempt_retry = event.details.will_attempt_retry
+
+                listRow = [id, created_at, resource_type, action, customer_notifications, cause, description, origin,
                                reason_code, scheme, will_attempt_retry,
                                mandate , new_customer_bank_account , new_mandate ,organisation , parent_event , payment ,
                                payout, previous_customer_bank_account ,refund ,subscription]
-                               ##EnsekID, EnsekStatementId]
-                    q.put(listRow)
-            except (json.decoder.JSONDecodeError, gocardless_pro.errors.GoCardlessInternalError, gocardless_pro.errors.MalformedResponseError) as e:
-                pass
-            data = q.queue
-            for d in data:
-                datalist.append(d)
-            # Empty queue
-            with q.mutex:
-                q.queue.clear()
+                q.put(listRow)
+
+        except (json.decoder.JSONDecodeError, gocardless_pro.errors.GoCardlessInternalError,
+                gocardless_pro.errors.MalformedResponseError) as e:
+            pass
+
+        while not q.empty():
+            datalist.append(q.get())
+
 
         df = pd.DataFrame(datalist, columns=['id',  'created_at', 'resource_type', 'action', 'customer_notifications',
                                              'cause', 'description', 'origin', 'reason_code', 'scheme', 'will_attempt_retry',
                                              'mandate' , 'new_customer_bank_account' , 'new_mandate' , 'organisation' ,
                                              'parent_event' , 'payment' , 'payout', 'previous_customer_bank_account' , 'refund' ,
-                                             'subscription']) ### , 'EnsekID', 'StatementId'])
+                                             'subscription'])
 
         print(df.head(5))
 
 
-        #df.to_csv('go_cardless_events_202001.csv', encoding='utf-8', index=False)
         df_string = df.to_csv(None, index=False)
         # print(df_account_transactions_string)
 
@@ -203,16 +185,26 @@ class GoCardlessEvents(object):
         print(s3.key)
         s3.set_contents_from_string(df_string)
 
+        # df.to_csv('go_cardless_Events.csv', encoding='utf-8', index=False)
+
+        return df
+
+    def sampletest(self):
+        for single_date in self.daterange():
+            print(single_date)
+
 
 if __name__ == "__main__":
-
     freeze_support()
-    s3 = db.get_S3_Connections_client()
+    s3 = db.get_finance_S3_Connections_client()
+    ### StartDate & EndDate in YYYY-MM-DD format ###
+    p = GoCardlessEvents('2020-02-04', '2020-02-05')
 
-    p = GoCardlessEvents('2020-01-01', 1)
+    p1 = p.process_Events()
 
-    p.process_Events()
-    ###print(fre + "T00:00:00.000Z")
+
+
+
 
 
 
