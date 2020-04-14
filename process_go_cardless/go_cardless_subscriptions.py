db1fb3d416f45a6ed42ece9218881f233a58dd04
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-04-14 11:32:08 +0100

    DMRE-1184

--- process_go_cardless/go_cardless_subscriptions.py
+++ process_go_cardless/go_cardless_subscriptions.py
@@ -22,31 +22,41 @@ from conf import config as con
 from connections.connect_db import get_finance_S3_Connections as s3_con
 from connections import connect_db as db
 
-client = gocardless_pro.Client(access_token='live_yk11i3mKMHN454DYUjIR0Mw6y34ePfW1GJoEXHaS', environment='live')
+client = gocardless_pro.Client(access_token=con.go_cardless['access_token'],
+                               environment=con.go_cardless['environment'])
 subscriptions = client.subscriptions
 
 
 class GoCardlessSubscriptions(object):
 
-    def __init__(self, execStartDate, execEndDate):
+    def __init__(self, _execStartDate=None, _execEndDate=None):
         self.env = util.get_env()
         self.dir = util.get_dir()
         self.bucket_name = self.dir['s3_finance_bucket']
         self.s3 = s3_con(self.bucket_name)
         self.fileDirectory = self.dir['s3_finance_goCardless_key']['Subscriptions']
         self.subscriptions = subscriptions
-        self.execStartDate = datetime.strptime(execStartDate, '%Y-%m-%d')
-        self.execEndDate = datetime.strptime(execEndDate, '%Y-%m-%d')
-        self.qtr = math.ceil(self.execStartDate.month / 3.)
-        self.yr = math.ceil(self.execStartDate.year)
-        self.s3key = 'timestamp=' + str(self.yr) + '-Q' + str(self.qtr)
-        self.filename = 'go_cardless_subscriptions_' + '{:%Y%m}'.format(self.execStartDate) + '_' + '{:%Y%m}'.format(
-            self.execEndDate) + '.csv'
+        self.toDay = datetime.today().strftime('%Y-%m-%d')
+        if _execStartDate is None:
+            _execStartDate = self.get_date(self.toDay, _addDays=-1)
+        self.execStartDate = datetime.strptime(_execStartDate, '%Y-%m-%d')
+        if _execEndDate is None:
+            _execEndDate = self.toDay
+        self.execEndDate = datetime.strptime(_execEndDate, '%Y-%m-%d')
 
-    def get_date(self, _date, dateFormat="%Y-%m-%d"):
+    def is_json(self, myjson):
+        try:
+            json_object = json.loads(myjson)
+        except ValueError as e:
+            return False
+        return True
+
+    def get_date(self, _date, _addDays=None, dateFormat="%Y-%m-%d"):
         dateStart = _date
         dateStart = datetime.strptime(dateStart, '%Y-%m-%d')
-        addDays = 1  ###self.noDays
+        if _addDays is None:
+            _addDays = 1  ###self.noDays
+        addDays = _addDays
         if (addDays != 0):
             dateEnd = dateStart + timedelta(days=addDays)
         else:
@@ -62,17 +72,28 @@ class GoCardlessSubscriptions(object):
             yield seq_date.strftime(dateFormat)
         return seq_date
 
-    def process_Subscriptions(self):
+    def process_Subscriptions(self, _StartDate=None, _EndDate=None):
         fileDirectory = self.fileDirectory
         s3 = self.s3
+        if _StartDate is None:
+            _StartDate = '{:%Y-%m-%d}'.format(self.execStartDate)
+        if _EndDate is None:
+            _EndDate = '{:%Y-%m-%d}'.format(self.execEndDate)
+        startdatetime = datetime.strptime(_StartDate, '%Y-%m-%d')
         subscriptions = self.subscriptions
+        filename = 'go_cardless_subscriptions_' + _StartDate + '_' + _EndDate + '.csv'
+        qtr = math.ceil(startdatetime.month / 3.)
+        yr = math.ceil(startdatetime.year)
+        s3key = 'timestamp=' + str(yr) + '-Q' + str(qtr)
+        print('Listing Subscriptions.......')
         # Loop through a page
         q = Queue()
         df_out = pd.DataFrame()
         datalist = []
         ls = []
-        StartDate = '{:%Y-%m-%d}'.format(self.execStartDate) + "T00:00:00.000Z"
-        EndDate = '{:%Y-%m-%d}'.format(self.execEndDate) + "T00:00:00.000Z"
+        StartDate = _StartDate + "T00:00:00.000Z"
+        EndDate = _EndDate + "T00:00:00.000Z"
+        print(_StartDate, _EndDate)
         for subscription in subscriptions.all(
                 params={"created_at[gte]": StartDate, "created_at[lte]": EndDate}):
             test = []
@@ -122,26 +143,33 @@ class GoCardlessSubscriptions(object):
         df_string = df.to_csv(None, index=False)
         # print(df_account_transactions_string)
 
-        s3.key = fileDirectory + os.sep + self.s3key + os.sep + self.filename
+        s3.key = fileDirectory + os.sep + s3key + os.sep + filename
         print(s3.key)
         s3.set_contents_from_string(df_string)
 
-        # df.to_csv('go_cardless_subscriptions.csv', encoding='utf-8', index=False)
         return df
 
-    def sampletest(self):
+    def runDailyFiles(self):
         for single_date in self.daterange():
-            print(single_date)
+            start = single_date
+            end = self.get_date(start)
+            ## print(start, end)
+            ### Execute Job ###
+            self.process_Subscriptions(start, end)
 
 
 if __name__ == "__main__":
     freeze_support()
     s3 = db.get_finance_S3_Connections_client()
     ### StartDate & EndDate in YYYY-MM-DD format ###
-    p = GoCardlessSubscriptions('2020-04-01', '2020-07-01')
+    ### When StartDate & EndDate is not provided it defaults to SysDate and Sysdate + 1 respectively ###
+    ### 2019-05-29 2019-05-30 ###
+    ##p = GoCardlessSubscriptions('2017-01-01', '2017-04-13')
+    p = GoCardlessSubscriptions()
 
     p1 = p.process_Subscriptions()
-
+    ### Extract return single Daily Files from Date Range Provided ###
+    ##p2 = p.runDailyFiles()
 
 
 
