c42fe9b58863013c63d5d6dd8434752080884a70
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-04-14 12:31:26 +0100

    DMRE-1184

--- process_go_cardless/go_cardless_mandates.py
+++ process_go_cardless/go_cardless_mandates.py
@@ -1,65 +1,62 @@
+import os
+import gocardless_pro
 import timeit
 
-import boto3
-import glob
 import pandas as pd
 import numpy as np
 import requests
 import json
-from pandas.io.json import json_normalize
-from ratelimit import limits, sleep_and_retry
-import time
-from requests import ConnectionError
-import csv
-import multiprocessing
-from time import sleep
 from multiprocessing import freeze_support
 from datetime import datetime, date, time, timedelta
 import math
 
-import os
-import gocardless_pro
 from queue import Queue
-import queue
-import requests
+from pandas.io.json import json_normalize
 
 import sys
-import os
 
 sys.path.append('..')
 
 from common import utils as util
 from conf import config as con
-from connections.connect_db import get_boto_S3_Connections as s3_con
+from connections.connect_db import get_finance_S3_Connections as s3_con
 from connections import connect_db as db
+
+client = gocardless_pro.Client(access_token=con.go_cardless['access_token'],
+                               environment=con.go_cardless['environment'])
+mandates = client.mandates
 
 
 class GoCardlessMandates(object):
 
-    def __init__(self, execDate=datetime.now(), noDays=1):
+    def __init__(self, _execStartDate=None, _execEndDate=None):
         self.env = util.get_env()
         self.dir = util.get_dir()
         self.bucket_name = self.dir['s3_finance_bucket']
         self.s3 = s3_con(self.bucket_name)
-        self.now = datetime.now()
-        self.execDate = datetime.strptime(execDate, '%Y-%m-%d')
-        self.qtr = math.ceil(self.execDate.month / 3.)
-        self.yr = math.ceil(self.execDate.year)
-        self.s3key = 'timestamp=' + str(self.yr) + '-Q' + str(self.qtr)
-        self.filename = 'go_cardless_Mandates_' + '{:%Y-%m-%d}'.format(self.execDate) + '.csv'
-        self.noDays = noDays
         self.fileDirectory = self.dir['s3_finance_goCardless_key']['Mandates']
+        self.mandates = mandates
+        self.toDay = datetime.today().strftime('%Y-%m-%d')
+        if _execStartDate is None:
+            _execStartDate = self.get_date(self.toDay, _addDays=-1)
+        self.execStartDate = datetime.strptime(_execStartDate, '%Y-%m-%d')
+        if _execEndDate is None:
+            _execEndDate = self.toDay
+        self.execEndDate = datetime.strptime(_execEndDate, '%Y-%m-%d')
 
-    def is_json(myjson):
+    def is_json(self, myjson):
         try:
             json_object = json.loads(myjson)
         except ValueError as e:
             return False
         return True
 
-    def get_date(self, dateFormat="%Y-%m-%d"):
-        dateStart = self.execDate
-        addDays = self.noDays
+    def get_date(self, _date, _addDays=None, dateFormat="%Y-%m-%d"):
+        dateStart = _date
+        dateStart = datetime.strptime(dateStart, '%Y-%m-%d')
+        if _addDays is None:
+            _addDays = 1  ###self.noDays
+        addDays = _addDays
         if (addDays != 0):
             dateEnd = dateStart + timedelta(days=addDays)
         else:
@@ -67,31 +64,42 @@ class GoCardlessMandates(object):
 
         return dateEnd.strftime(dateFormat)
 
-    def process_Mandates(self):
-        bucket_name = self.bucket_name
-        execStartDate = '{:%Y-%m-%d}'.format(self.execDate) + "T00:00:00.000Z"
-        execEndDate = self.get_date() + "T00:00:00.000Z"
-        s3 = self.s3
-        dir_s3 = self.dir
-        fileDirectory = self.fileDirectory
+    def daterange(self, dateFormat="%Y-%m-%d"):
+        start_date = self.execStartDate
+        end_date = self.execEndDate
+        for n in range(int((end_date - start_date).days)):
+            seq_date = start_date + timedelta(n)
+            yield seq_date.strftime(dateFormat)
+        return seq_date
 
-        client = gocardless_pro.Client(access_token=con.go_cardless['access_token'],
-                                       environment=con.go_cardless['environment'])
+    def process_Mandates(self, _StartDate=None, _EndDate=None):
+        fileDirectory = self.fileDirectory
+        s3 = self.s3
+        if _StartDate is None:
+            _StartDate = '{:%Y-%m-%d}'.format(self.execStartDate)
+        if _EndDate is None:
+            _EndDate = '{:%Y-%m-%d}'.format(self.execEndDate)
+        startdatetime = datetime.strptime(_StartDate, '%Y-%m-%d')
+        mandates = self.mandates
+        filename = 'go_cardless_mandates_' + _StartDate + '_' + _EndDate + '.csv'
+        qtr = math.ceil(startdatetime.month / 3.)
+        yr = math.ceil(startdatetime.year)
+        s3key = 'timestamp=' + str(yr) + '-Q' + str(qtr)
         # Loop through a page
         q = Queue()
+        df_out = pd.DataFrame()
         datalist = []
-
-        # Fetch a mandate by its ID
-        mandate = client.mandates
-
-        # Loop through a page of payments, printing each payment's amount
+        ls = []
+        StartDate = _StartDate + "T00:00:00.000Z"
+        EndDate = _EndDate + "T00:00:00.000Z"
+        print(_StartDate, _EndDate)
         print('.....listing mandates')
-        df_mandate = pd.DataFrame()
+
         for mandate in client.mandates.all(
                 params={"created_at[gte]": "2020-01-01T00:00:00.000Z", "created_at[lte]": "2020-03-01T00:00:00.000Z"}):
-            EnsekAccountId = ''
-            StatementId = ''
-            print(mandate.id)
+            EnsekAccountId = None
+            StatementId = None
+            #print(mandate.id)
             if 'AccountId' in mandate.metadata:
                 EnsekAccountId = mandate.metadata['AccountId']
             if 'StatementId' in mandate.metadata:
@@ -127,15 +135,33 @@ class GoCardlessMandates(object):
         df_string = df.to_csv(None, index=False)
         # print(df_account_transactions_string)
 
-        s3.key = fileDirectory + os.sep + self.s3key + os.sep + self.filename
+        s3.key = fileDirectory + os.sep + s3key + os.sep + filename
         print(s3.key)
         s3.set_contents_from_string(df_string)
+
+        return df
+
+    def runDailyFiles(self):
+        for single_date in self.daterange():
+            start = single_date
+            end = self.get_date(start)
+            ## print(start, end)
+            ### Execute Job ###
+            self.process_Mandates(start, end)
 
 
 if __name__ == "__main__":
     freeze_support()
-    s3 = db.get_S3_Connections_client()
+    s3 = db.get_finance_S3_Connections_client()
+    ### StartDate & EndDate in YYYY-MM-DD format ###
+    ### When StartDate & EndDate is not provided it defaults to SysDate and Sysdate + 1 respectively ###
+    ### 2019-05-29 2019-05-30 ###
+    ##p = GoCardlessmandates('2017-01-01', '2017-04-13')
+    p = GoCardlessMandates()
 
-    p = GoCardlessMandates('2020-01-01', 1)
+    p1 = p.process_Mandates()
+    ### Extract return single Daily Files from Date Range Provided ###
+    ##p2 = p.runDailyFiles()
 
-    p.process_Mandates()
+
+
