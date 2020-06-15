13d9b3db4a57a4633e2cfbcc75b87caed47365dd
Jonathan Steel <jonathan.steel@igloo.energy>
2020-06-15 15:38:19 +0100

    INF-208

--- process_Ensek/processEnsekTariffs/process_ensek_tariffs_history.py
+++ process_Ensek/processEnsekTariffs/process_ensek_tariffs_history.py
@@ -197,44 +197,46 @@ if __name__ == "__main__":
     # p.processAccounts(account_ids, s3, dir_s3)
 
     ####### Multiprocessing Starts #########
-    env = util.get_env()
-    total_processes = util.get_multiprocess('total_ensek_processes')
+    if not account_ids:
+        env = util.get_env()
+        total_processes = util.get_multiprocess('total_ensek_processes')
 
-    if env == 'uat':
-        n = total_processes  # number of process to run in parallel
-    else:
-        n = total_processes
-
-    k = int(len(account_ids) / n)  # get equal no of files for each process
-
-    print(len(account_ids))
-    print(k)
-
-    processes = []
-    lv = 0
-    start = timeit.default_timer()
-
-    for i in range(n + 1):
-        p1 = TariffHistory()
-        print(i)
-        uv = i * k
-        if i == n:
-            # print(d18_keys_s3[l:])
-            t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:], s3_con(bucket_name), dir_s3))
+        if env == 'uat':
+            n = total_processes  # number of process to run in parallel
         else:
-            # print(d18_keys_s3[l:u])
-            t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:uv], s3_con(bucket_name), dir_s3))
-        lv = uv
+            n = total_processes
 
-        processes.append(t)
+        k = int(len(account_ids) / n)  # get equal no of files for each process
 
-    for p in processes:
-        p.start()
-        time.sleep(2)
+        print(len(account_ids))
+        print(k)
 
-    for process in processes:
-        process.join()
-    ####### Multiprocessing Ends #########
+        processes = []
+        lv = 0
+        start = timeit.default_timer()
 
-    print("Process completed in " + str(timeit.default_timer() - start) + ' seconds')
+        for i in range(n + 1):
+            p1 = TariffHistory()
+            print(i)
+            uv = i * k
+            if i == n:
+                # print(d18_keys_s3[l:])
+                t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:], s3_con(bucket_name), dir_s3))
+            else:
+                # print(d18_keys_s3[l:u])
+                t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:uv], s3_con(bucket_name), dir_s3))
+            lv = uv
+
+            processes.append(t)
+
+        for p in processes:
+            p.start()
+            time.sleep(2)
+
+        for process in processes:
+            process.join()
+        ####### Multiprocessing Ends #########
+
+    else:
+        print("Process completed in " + str(timeit.default_timer() - start) + ' seconds')
 
