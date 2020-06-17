4ccebe0443d14ec41e5215a3f68fb6d3a7358e6a
Jonathan Steel <jonathan.steel@igloo.energy>
2020-06-17 17:37:38 +0100

    INF- 213

---
+++ external_weather_historical-DAG.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.bash_operator import BashOperator
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2), # don't know what this is doing
+}
+
+dag = DAG(
+    dag_id='igloo_weather_historical',
+    default_args=args,
+    schedule_interval=None,
+    tags=['cdw']
+)
+
+start_weather_processing_jobs = BashOperator(
+    task_id='start_weather_processing_jobs',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_WeatherData && ../.venv/bin/python start_weather_processing_jobs.py',
+    dag=dag,
+)
+
+start_weather_staging_jobs = BashOperator(
+    task_id='start_weather_staging_jobs',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_WeatherData && ../.venv/bin/python start_weather_staging_jobs.py',
+    dag=dag,
+)
+
+start_weather_ref_jobs = BashOperator(
+    task_id='start_weather_ref_jobs',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_WeatherData && ../.venv/bin/python start_weather_ref_jobs.py',
+    dag=dag,
+)
+
+start_weather_processing_jobs >> start_weather_staging_jobs >> start_weather_ref_jobs
