0bd85fee9963322621c4d8b14c8783de65e61d42
Jonathan Steel <jonathan.steel@igloo.energy>
2020-06-17 15:00:38 +0100

    INF-208 Test the correction

--- ensek_pa-DAG.py
+++ ensek_pa-DAG.py
@@ -48,4 +48,6 @@ start_ensek_pa_ref_jobs = BashOperator(
     dag=dag,
 )
 
-process_customerdb >> process_ensek_meterpoints_no_history >> start_ensek_pa_staging_jobs >> start_ensek_pa_ref_jobs
+#process_customerdb >> process_ensek_meterpoints_no_history >> start_ensek_pa_staging_jobs >> start_ensek_pa_ref_jobs
+
+process_ensek_meterpoints_no_history >> start_ensek_pa_staging_jobs >> start_ensek_pa_ref_jobs
