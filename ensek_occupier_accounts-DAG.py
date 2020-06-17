fbb8330d30602f2d98a57a8e96d19d21390e0c9c
Jonathan Steel <jonathan.steel@igloo.energy>
2020-06-17 12:46:40 +0100

    INF 246

---
+++ ensek_occupier_accounts-DAG.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.bash_operator import BashOperator
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2), # don't know what this is doing
+}
+
+dag = DAG(
+    dag_id='ensek_occupier_accounts',
+    default_args=args,
+    schedule_interval=None,
+    tags=['cdw']
+)
+
+process_ensek_occupier_accounts = BashOperator(
+    task_id='process_ensek_occupier_accounts',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_Ensek/processEnsekOccupierAccounts && ../.venv/bin/python process_ensek_occupier_accounts.py',
+    dag=dag,
+)
+
+start_land_registry_staging_jobs = BashOperator(
+    task_id='start_ensek_occupier_accounts_staging_jobs',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_Ensek/processEnsekOccupierAccounts && ../.venv/bin/python start_ensek_occupier_accounts_staging_jobs.py',
+    dag=dag,
+)
+
+start_ensek_occupier_accounts_ref_jobs = BashOperator(
+    task_id='start_ensek_occupier_accounts_ref_jobs',
+    bash_command='cd /usr/local/airflow/dags/enzek-meterpoint-readings/process_Ensek/processEnsekOccupierAccounts && ../.venv/bin/python start_ensek_occupier_accounts_ref_jobs.py',
+    dag=dag,
+)
+
+process_ensek_occupier_accounts >> start_land_registry_staging_jobs >> start_ensek_occupier_accounts_ref_jobs
