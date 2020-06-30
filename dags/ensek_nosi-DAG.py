0a0fc03e67094043ba8b042b6fd07afb82aec3fd
Jonathan Steel <jonathan.steel@igloo.energy>
2020-06-30 11:05:39 +0100

    INF-254 Ref Glue Version

---
+++ dags/ensek_nosi-DAG.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.bash_operator import BashOperator
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2), # don't know what this is doing
+}
+
+dag = DAG(
+    dag_id='ensek_nosi',
+    default_args=args,
+    schedule_interval=None,
+    tags=['cdw']
+)
+
+download_nosi = BashOperator(
+    task_id='download_nosi',
+    bash_command='cd /opt/code/enzek-meterpoint-readings/process_Nosi && ../.venv/bin/python download_nosi.py',
+    dag=dag,
+)
+
+start_ensek_readings_nosi_staging_jobs = BashOperator(
+    task_id='start_ensek_readings_nosi_staging_jobs',
+    bash_command='cd /opt/code/enzek-meterpoint-readings/process_Nosi && ../.venv/bin/python start_ensek_readings_nosi_staging_jobs.py',
+    dag=dag,
+)
+
+start_ensek_readings_nosi_ref_jobs = BashOperator(
+    task_id='start_ensek_readings_nosi_ref_jobs',
+    bash_command='cd /opt/code/enzek-meterpoint-readings/process_Nosi && ../.venv/bin/python start_ensek_readings_nosi_ref_jobs.py',
+    dag=dag,
+)
+
+download_nosi >> start_ensek_readings_nosi_staging_jobs >> start_ensek_readings_nosi_ref_jobs
