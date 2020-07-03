44f3f196dccc7ecc4af6abf8980fd17d1b002ede
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-07-03 10:06:19 +0100

    DMRE-1172

--- process_go_cardless/go_cardless_payout_deprecated.py
+++ process_go_cardless/go_cardless_payout_deprecated.py
@@ -35,11 +35,15 @@ class GoCardlessPayouts(object):
         self.dir = util.get_dir()
         self.bucket_name = self.dir['s3_finance_bucket']
         self.s3 = s3_con(self.bucket_name)
-        self.sql = 'select max(created_at) as lastRun from aws_fin_stage1_extracts.fin_go_cardless_api_payouts '
         self.fileDirectory = self.dir['s3_finance_goCardless_key']['Payouts']
         self.payouts = payouts
-        self.execEndDate = datetime.now().replace(microsecond=0).isoformat() ##datetime.today().strftime('%Y-%m-%d')
         self.toDay = datetime.today().strftime('%Y-%m-%d')
+        if _execStartDate is None:
+            _execStartDate = self.get_date(self.toDay, _addDays = -1)
+        self.execStartDate = datetime.strptime(_execStartDate, '%Y-%m-%d')
+        if _execEndDate is None:
+            _execEndDate = self.toDay
+        self.execEndDate = datetime.strptime(_execEndDate, '%Y-%m-%d')
 
     def is_json(self, myjson):
         try:
@@ -62,23 +66,26 @@ class GoCardlessPayouts(object):
 
         return dateEnd.strftime(dateFormat)
 
-    def daterange(self, start_date, dateFormat="%Y-%m-%d"):
-        end_date = datetime.strptime(self.execEndDate, '%Y-%m-%d')   ##self.execEndDate
+    def daterange(self, dateFormat="%Y-%m-%d"):
+        start_date = self.execStartDate
+        end_date = self.execEndDate
         for n in range(int((end_date - start_date).days)):
             seq_date = start_date + timedelta(n)
             yield seq_date.strftime(dateFormat)
         return seq_date
 
-    def process_Payouts(self, _StartDate, _EndDate):
+    def process_Payouts(self, _StartDate = None, _EndDate = None):
         fileDirectory = self.fileDirectory
         s3 = self.s3
-        RpStartDate = _StartDate
-        RpEndDate = _EndDate
+        if _StartDate is None:
+            _StartDate = '{:%Y-%m-%d}'.format(self.execStartDate)
+        if _EndDate is None:
+            _EndDate = '{:%Y-%m-%d}'.format(self.execEndDate)
+        startdatetime = datetime.strptime(_StartDate, '%Y-%m-%d')
         payouts = self.payouts
         filename = 'go_cardless_payouts_' + _StartDate + '_' + _EndDate + '.csv'
-        fileDate = datetime.strptime(self.toDay, '%Y-%m-%d')
-        qtr = math.ceil(fileDate.month / 3.)
-        yr = math.ceil(fileDate.year)
+        qtr = math.ceil(startdatetime.month / 3.)
+        yr = math.ceil(startdatetime.year)
         fkey = 'timestamp=' + str(yr) + '-Q' + str(qtr) + '/'
         print('Listing payouts.......')
         # Loop through a page
@@ -86,11 +93,13 @@ class GoCardlessPayouts(object):
         df_out = pd.DataFrame()
         datalist = []
         ls = []
-        print(RpStartDate, _EndDate)
+        StartDate = _StartDate + "T00:00:00.000Z"
+        EndDate = _EndDate + "T00:00:00.000Z"
+        print(_StartDate, _EndDate)
 
         # Loop through a page of Payouts, printing each Payout's amount
         print('.....listing payouts')
-        for payout in client.payouts.all(params={"created_at[gt]": RpStartDate, "created_at[lte]": RpEndDate}):
+        for payout in client.payouts.all(params={"created_at[gte]": StartDate, "created_at[lte]": EndDate}):
             payout_id = payout.id
             amount = payout.amount
             arrival_date = payout.arrival_date
@@ -116,23 +125,37 @@ class GoCardlessPayouts(object):
 
         df_string = df.to_csv(None, index=False)
         # print(df_account_transactions_string)
+
+        ## s3.key = fileDirectory + os.sep + s3key + os.sep + filename
+        ## s3.key = Path(fileDirectory, s3key, filename)
         s3.key = fileDirectory + fkey + filename
         print(s3.key)
         s3.set_contents_from_string(df_string)
 
+        # df.to_csv('go_cardless_payouts.csv', encoding='utf-8', index=False)
+
         return df
+
+    def runDailyFiles(self):
+        for single_date in self.daterange():
+            start = single_date
+            end = self.get_date(start)
+            ## print(start, end)
+            ### Execute Job ###
+            self.process_Payouts(start, end)
 
 if __name__ == "__main__":
     freeze_support()
     s3 = db.get_finance_S3_Connections_client()
+    ### StartDate & EndDate in YYYY-MM-DD format ###
+    ### When StartDate & EndDate is not provided it defaults to SysDate and Sysdate + 1 respectively ###
+    ### 2019-05-29 2019-05-30 ###
+    ##p = GoCardlessPayouts('2020-04-01', '2020-07-01')
     p = GoCardlessPayouts()
-    startdateDF = util.execute_query(p.sql)
-    ReportEndDate = str(p.execEndDate) + str(".000Z")
-    ReportStartDate = str(startdateDF.iat[0,0])
-    print('ReportStartDate:  {0}'.format(ReportStartDate))
-    print('ReportEndDate:  {0}'.format(ReportEndDate))
 
-    p1 = p.process_Payouts(_StartDate=ReportStartDate, _EndDate=ReportEndDate)
+    p1 = p.process_Payouts()
+    ### Extract return single Daily Files from Date Range Provided ###
+    ## p2 = p.runDailyFiles()
 
 
 
