65292cdffeaa6ddcfb67e8906f0e2cb23b938df4
Jonathan Steel <jonathan.steel@igloo.energy>
2020-07-16 15:58:33 +0100

    INF-266 Adjusted Cron Schedule

--- dags/ensek_pa-DAG.py
+++ dags/ensek_pa-DAG.py
@@ -20,7 +20,7 @@ args = {
 dag = DAG(
     dag_id='ensek_pa',
     default_args=args,
-    schedule_interval='45 16 * * *',
+    schedule_interval='45 17 * * *',
     tags=['cdw']
 )
 
--- dags/external_land_registry-DAG.py
+++ dags/external_land_registry-DAG.py
@@ -20,7 +20,7 @@ args = {
 dag = DAG(
     dag_id='igloo_land_registry',
     default_args=args,
-    schedule_interval='45 15 * * *',
+    schedule_interval='15 16 * * *',
     tags=['cdw']
 )
 
