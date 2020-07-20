e59991d30176f80713f9baac8b85b24944f49ecd
Jonathan Steel <jonathan.steel@igloo.energy>
2020-07-20 14:02:30 +0100

    INF-266 Trying to get the Ensek NOn PA full job running correctly

--- dags/ensek_non_pa-DAG.py
+++ dags/ensek_non_pa-DAG.py
@@ -24,35 +24,12 @@ dag = DAG(
     tags=['cdw']
 )
 
-process_ensek_registration_meterpoint_status = BashOperator(
-    task_id='process_ensek_registration_meterpoint_status.py',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python processEnsekStatus/process_ensek_registration_meterpoint_status.py',
+start_ensek_api_mirror_only_jobs = BashOperator(
+    task_id='start_ensek_api_mirror_only_jobs',
+    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python start_ensek_api_mirror_only_jobs.py',
     dag=dag,
 )
 
-process_ensek_internal_estimates = BashOperator(
-    task_id='process_ensek_internal_estimates',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python processEnsekEstimates/process_ensek_internal_estimates.py',
-    dag=dag,
-)
-
-process_ensek_tariffs_history = BashOperator(
-    task_id='process_ensek_tariffs_history',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python processEnsekTariffs/process_ensek_tariffs_history.py',
-    dag=dag,
-)
-
-process_ensek_account_settings = BashOperator(
-    task_id='process_ensek_account_settings',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python processEnsekAccountSettings/process_ensek_account_settings.py',
-    dag=dag,
-)
-
-process_ensek_transactions = BashOperator(
-    task_id='process_ensek_transactions',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python processEnsekTransactions/process_ensek_transactions.py',
-    dag=dag,
-)
 
 start_ensek_non_pa_staging_jobs = BashOperator(
     task_id='start_ensek_non_pa_staging_jobs',
@@ -66,10 +43,4 @@ start_ensek_non_pa_ref_jobs = BashOperator(
     dag=dag,
 )
 
-start_ensek_api_mirror_only_jobs = BashOperator(
-    task_id='start_ensek_api_mirror_only_jobs',
-    bash_command='cd /opt/airflow/enzek-meterpoint-readings/process_Ensek && python start_ensek_api_mirror_only_jobs.py',
-    dag=dag,
-)
-
-process_ensek_registration_meterpoint_status >> process_ensek_internal_estimates >> process_ensek_tariffs_history >> process_ensek_account_settings >> process_ensek_transactions >> start_ensek_api_mirror_only_jobs >> start_ensek_non_pa_staging_jobs >> start_ensek_non_pa_ref_jobs
+start_ensek_api_mirror_only_jobs >> start_ensek_non_pa_staging_jobs >> start_ensek_non_pa_ref_jobs
