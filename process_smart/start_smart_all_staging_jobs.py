9c8dcfb55a9db3767ffa1e35e1048be503ab3244
Jonathan Steel <jonathan.steel@igloo.energy>
2020-08-14 14:15:36 +0100

    DMRE-1405  Corrected logging in the staging job

--- process_smart/start_smart_all_staging_jobs.py
+++ process_smart/start_smart_all_staging_jobs.py
@@ -11,25 +11,20 @@ from common import Refresh_UAT as refresh
 from process_calculated_steps import start_calculated_steps_jobs as sj
 
 
-class ALP:
+class SmartStagingAll:
     def __init__(self):
-        self.process_name = "SMart All Staging "
+        self.process_name = "Smart All Staging "
         self.pythonAlias = util.get_pythonAlias()
         self.env = util.get_env()
         self.dir = util.get_dir()
 
         self.all_jobid = util.get_jobID()
-
-        self.alp_wcf_jobid = util.get_jobID()
-        self.alp_cv_jobid = util.get_jobID()
-        self.alp_wcf_staging_jobid = util.get_jobID()
-        self.alp_cv_staging_jobid = util.get_jobID()
-        self.alp_ref_jobid = util.get_jobID()
+        self.smart_all_staging_jobid = util.get_jobID()
 
 
     def submit_smart_all_staging_gluejob(self):
         try:
-            util.batch_logging_insert(self.alp_cv_staging_jobid, 601, 'smart_all_staging_glue_job','start_smart_all_staging_jobs.py')
+            util.batch_logging_insert(self.smart_all_staging_jobid, 601, 'smart_all_staging_glue_job','start_smart_all_staging_jobs.py')
             jobName = self.dir['glue_staging_job_name']
             s3_bucket = self.dir['s3_bucket']
             environment = self.env
@@ -37,7 +32,7 @@ class ALP:
             obj_stage = glue.ProcessGlueJob(job_name=jobName, s3_bucket=s3_bucket, environment=environment, processJob='smart-all')
             staging_job_response = obj_stage.run_glue_job()
             if staging_job_response:
-                util.batch_logging_update(self.alp_cv_staging_jobid, 'e')
+                util.batch_logging_update(self.smart_all_staging_jobid, 'e')
                 print("{0}: Staging Job Completed successfully for {1}".format(datetime.now().strftime('%H:%M:%S'),self.process_name))
             else:
                 print("Error occurred in {0} Staging Job".format(self.process_name))
@@ -45,7 +40,7 @@ class ALP:
                 raise Exception
 
         except Exception as e:
-            util.batch_logging_update(self.alp_cv_staging_jobid, 'f', str(e))
+            util.batch_logging_update(self.smart_all_staging_jobid, 'f', str(e))
             util.batch_logging_update(self.all_jobid, 'f', str(e))
             print("Error in Staging Job :- " + str(e))
             sys.exit(1)
@@ -55,9 +50,9 @@ class ALP:
 
 if __name__ == '__main__':
 
-    s = ALP()
+    s = SmartStagingAll()
 
-    util.batch_logging_insert(s.all_jobid, 600, 'all_alp_jobs', 'start_alp_jobs.py')
+    util.batch_logging_insert(s.all_jobid, 600, 'all_smart_all_jobs', 'start_smart_all_staging_jobs.py')
 
     #
     # # run alp wcf staging glue job
