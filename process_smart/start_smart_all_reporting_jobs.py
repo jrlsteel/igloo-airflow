29ef24a4db5eee31c4f90f9b7399a8f5350c7ea3
Jonathan Steel <jonathan.steel@igloo.energy>
2020-09-28 11:49:22 +0100

    DMRE-1553

---
+++ process_smart/start_smart_all_reporting_jobs.py
@@ -0,0 +1,50 @@
+import sys
+from datetime import datetime
+
+sys.path.append('..')
+from common import process_glue_job as glue
+from common import utils as util
+
+
+class ReportingJobs:
+    def __init__(self):
+        self.env = util.get_env()
+        self.dir = util.get_dir()
+        self.reporting_jobid = util.get_jobID()
+
+    def submit_daily_reporting_batch_gluejob(self):
+        try:
+            job_name = self.dir['glue_reporting_job_name']
+            s3_bucket = self.dir['s3_bucket']
+            environment = self.env
+
+            # Batch Logging
+            util.batch_logging_insert(self.reporting_jobid, 602, 'smart_reader_daily_reporting',
+                                      'start_reporting_jobs.py')
+
+            daily_reporting_job_response = glue.ProcessGlueJob(job_name=job_name,
+                                                               s3_bucket=s3_bucket,
+                                                               environment=environment,
+                                                               processJob='smart_reader_daily_reporting').run_glue_job()
+
+            if daily_reporting_job_response:
+                print("{0}: Daily Reporting Job Completed successfully".format(datetime.now().strftime('%H:%M:%S')))
+                # Batch Logging
+                util.batch_logging_update(self.reporting_jobid, 'e')
+            else:
+                print("Error occurred in Daily Reporting Job")
+                # return staging_job_response
+                raise Exception
+        except Exception as e:
+            print("Error in Daily Reporting Glue Job: " + str(e))
+
+            # Batch Logging
+            util.batch_logging_update(self.reporting_jobid, 'f', str(e))
+
+            # write
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    rj = ReportingJobs()
+    rj.submit_daily_reporting_batch_gluejob()
