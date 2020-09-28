8f2653461350597e99dcae163ec40fb2172496c3
Jonathan Steel <jonathan.steel@igloo.energy>
2020-09-28 12:12:29 +0100

    DMRE-1553

--- common/directories.py
+++ common/directories.py
@@ -121,6 +121,11 @@ uat = {
             "api_url": "https://9m73o40v3h.execute-api.eu-west-1.amazonaws.com/prod/Accounts/{0}/eac",
             "token": "quqSpLjJly3jlh79S7uUN9uE1YoPQums86o0768f"
         },
+        "smart_reads_billing": {
+            "api_url": "https://vpce-0fc7cde64b2850e80-lxb0i11z.execute-api.eu-west-1.vpce.amazonaws.com/staging/api/v1/meter-reads",
+            "api_key": "HorYWPvBNO6ULqsw3aZozyW7vJKJowlacPhTH8I6",
+            "host": "xcy0iyaa30.execute-api.eu-west-1.amazonaws.com"
+        },
 
     },
 
