f2ea01671985151ee006f36b86f60a0a278ba727
Jonathan Steel <jonathan.steel@igloo.energy>
2020-09-29 11:46:07 +0100

    DMRE-1545

--- process_smart/start_smart_all_jobs.py
+++ process_smart/start_smart_all_jobs.py
@@ -13,9 +13,9 @@ class SmartReadingProcessingJobs:
         self.pythonAlias = util.get_pythonAlias()
         self.env = util.get_env()
         self.dir = util.get_dir()
-        self.occupier_accounts_extract_jobid = util.get_jobID()
-        self.occupier_accounts_reference_jobid = util.get_jobID()
-        self.occupier_accounts_job_id = util.get_jobID()
+        self.start_smart_all_reporting_jobs_jobid = util.get_jobID()
+        self.start_smart_all_jobid = util.get_jobID()
+        self.start_smart_all_billing_jobs_jobid = util.get_jobID()
         self.jobName = self.dir['glue_estimated_advance_job_name']
 
     def submit_start_smart_all_reporting_jobs_job(self):
@@ -26,15 +26,16 @@ class SmartReadingProcessingJobs:
 
         print("{0}: >>>> start_smart_all_reporting_jobs    <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
-            util.batch_logging_insert(self.occupier_accounts_extract_jobid , 51, 'start_smart_all_reporting_jobs', 'start_ensek_occupier_accounts_jobs.py')
+            util.batch_logging_insert(self.start_smart_all_reporting_jobs_jobid, 51, 'start_smart_all_reporting_jobs',
+                                      'submit_start_smart_all_reporting_jobs_job.py')
             start = timeit.default_timer()
             subprocess.run([self.pythonAlias, "start_smart_all_reporting_jobs.py"], check=True)
             print("{0}: start_smart_all_reporting_jobs  completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
                                                                                float(timeit.default_timer() - start)))
-            util.batch_logging_update(self.occupier_accounts_extract_jobid, 'e')
+            util.batch_logging_update(self.start_smart_all_reporting_jobs_jobid, 'e')
         except Exception as e:
-            util.batch_logging_update(self.occupier_accounts_extract_jobid, 'f', str(e))
-            util.batch_logging_update(self.occupier_accounts_job_id, 'f', str(e))
+            util.batch_logging_update(self.start_smart_all_reporting_jobs_jobid, 'f', str(e))
+            util.batch_logging_update(self.start_smart_all_jobid, 'f', str(e))
             print("Error in start_smart_all_reporting_jobs  :- " + str(e))
             sys.exit(1)
 
@@ -46,15 +47,16 @@ class SmartReadingProcessingJobs:
 
         print("{0}: >>>> process_smart_reads_billing   <<<<".format(datetime.now().strftime('%H:%M:%S')))
         try:
-            util.batch_logging_insert(self.occupier_accounts_extract_jobid , 51, 'Occupier Accounts pyscript', 'start_ensek_occupier_accounts_jobs.py')
+            util.batch_logging_insert(self.start_smart_all_billing_jobs_jobid, 51, 'Smart Reads Billing pyscript',
+                                      'process_smart_reads_billing.py')
             start = timeit.default_timer()
             subprocess.run([self.pythonAlias, "process_smart_reads_billing.py"], check=True)
             print("{0}: process_smart_reads_billing  completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
                                                                                float(timeit.default_timer() - start)))
-            util.batch_logging_update(self.occupier_accounts_extract_jobid, 'e')
+            util.batch_logging_update(self.start_smart_all_billing_jobs_jobid, 'e')
         except Exception as e:
-            util.batch_logging_update(self.occupier_accounts_extract_jobid, 'f', str(e))
-            util.batch_logging_update(self.occupier_accounts_job_id, 'f', str(e))
+            util.batch_logging_update(self.start_smart_all_billing_jobs_jobid, 'f', str(e))
+            util.batch_logging_update(self.start_smart_all_jobid, 'f', str(e))
             print("Error in process_smart_reads_billing  :- " + str(e))
             sys.exit(1)
 
@@ -65,8 +67,8 @@ if __name__ == '__main__':
 
     s = SmartReadingProcessingJobs()
 
-    util.batch_logging_insert(s.occupier_accounts_job_id, 51, 'all_occupier_accounts_jobs',
-                              'start_smart_all_reporting_jobs_job.py')
+    util.batch_logging_insert(s.start_smart_all_jobid, 51, 'Smart All Jobs',
+                              'start_smart_all_jobs.py')
 
     # #start_smart_all_reporting_jobs_job
     print("{0}:  start_smart_all_reporting_jobs_job running...".format(datetime.now().strftime('%H:%M:%S')))
@@ -78,4 +80,4 @@ if __name__ == '__main__':
 
     print("{0}: SmartReadingProcessingJobs completed successfully".format(datetime.now().strftime('%H:%M:%S')))
 
-    util.batch_logging_update(s.occupier_accounts_job_id, 'e')
+    util.batch_logging_update(s.start_smart_all_jobid, 'e')
