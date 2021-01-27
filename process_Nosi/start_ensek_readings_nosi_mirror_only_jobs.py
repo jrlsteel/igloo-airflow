43973c6e9f2ac7635605a7cd33d64996ef72e428
alex geeson <alex.geeson@igloo.energy>
2021-01-27 12:35:18 +0000

    Merged in feature/MTRG-261 (pull request #88)
    fix: Reconfigured for new environment
    
    * fix: Reconfigured for new environemnt
    
    * Merged develop into feature/MTRG-261
    
    Approved-by: Jonathan Steel

--- process_Nosi/start_ensek_readings_nosi_mirror_only_jobs.py
+++ process_Nosi/start_ensek_readings_nosi_mirror_only_jobs.py
@@ -50,6 +50,24 @@ class StartReadingsNOSIJobs:
             print("Error in process :- " + str(e))
             sys.exit(1)
 
+    def submit_download_nosi_job(self):
+
+        pythonAlias = util.get_pythonAlias()
+
+        print("{0}: >>>> Downloading NOSI files <<<<".format(datetime.now().strftime('%H:%M:%S')))
+        try:
+            util.batch_logging_insert(self.nosi_download_jobid, 43, 'nosi_download_pyscript', 'start_ensek_readings_nosi_jobs.py')
+            start = timeit.default_timer()
+            subprocess.run([pythonAlias, "download_nosi.py"], check=True)
+            util.batch_logging_update(self.nosi_download_jobid, 'e')
+            print("{0}: download_NOSI completed in {1:.2f} seconds".format(datetime.now().strftime('%H:%M:%S'),
+                                                                          float(timeit.default_timer() - start)))
+        except Exception as e:
+            util.batch_logging_update(self.nosi_download_jobid, 'f', str(e))
+            util.batch_logging_update(self.all_jobid, 'f', str(e))
+            print("Error in download_NOSI process :- " + str(e))
+            sys.exit(1)
+
 
 if __name__ == '__main__':
 
@@ -60,7 +78,7 @@ if __name__ == '__main__':
     s3_destination_bucket = s.dir['s3_bucket']
     s3_source_bucket = s.dir['s3_source_bucket']
 
-    if s.env == 'prod':
+    if s.env == 'newprod':
         # # # Ensek Internal Readings NOSI Downloading
         print("{0}: download_NOSI job is running...".format(datetime.now().strftime('%H:%M:%S')))
         s.submit_download_nosi_job()
