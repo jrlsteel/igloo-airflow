ea0f1efd5d3ddea66ff96e011e4f90ad2836c6b8
alex geeson <alex.geeson@igloo.energy>
2021-03-18 11:22:38 +0000

    Merged in feature/MTRG-569 (pull request #112)
    Feature/MTRG-569
    
    * fix: alter schedule for non_pa dag as overlapping with other etls
    
    MTRG-549
    
    * fix: move forward non-pa dag start time by 2 hours in all envrionments
    
    * fix: non_pa_dag schedule moved forward by 1.5 hour for all environments
    
    * fix: non_pa_dag schedule moved forward by 1.5 hour for all environments
    
    * Changes pushed to develop no have to resolved.
    
    Approved-by: Jonathan Steel

--- common/schedules/dev.py
+++ common/schedules/dev.py
@@ -1,5 +1,5 @@
 schedules = {
-    'ensek_non_pa_test': '30 06 * * *',
+    'ensek_non_pa_test': '00 07 * * *',
     'ensek_nosi': '00 19 * * *',
     'ensek_nrl': '15 18 * * *',
     'ensek_occupier_accounts': '30 18 * * *',
