666246223ec19d4d5e5988e00932d8594fbeba64
Simon Hunt <simon.hunt@igloo.energy>
2021-04-22 08:57:22 +0000

    Merged in feature/ENGY-555 (pull request #124)
    Feature/ENGY-555: Pull Ensek's daily record of GoCardless links automatically
    
    * fix: prefix ssm parameters with /dag
    
    * fix: try removing aws credentials from SSM client initialisation
    
    * fix: use existing 'ensek_sftp_config' object in config
    
    * fix: hard-coded sftp_prefix as it's not in the config object
    
    * fix: change key names to correct ones in config
    
    * fix: add known_hosts file
    
    * fix: bugs of zero bytes file uploaded to s3 and today's file getting deleted if run already
    
    * fix: change from using BashOperator to PythonOperator with Sentry
    
    * fix: move known_hosts data into the code rather than loading from a file
    
    * fix: change finance bucket config back as it was for old environments
    
    Approved-by: Ed Crampin
    Approved-by: Louis Quaintance

---
+++ dags/external_go_cardless_report_download-DAG.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import time
+import sys
+from builtins import range
+from pprint import pprint
+
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.bash_operator import BashOperator
+
+sys.path.append("/opt/airflow/enzek-meterpoint-readings")
+
+from conf import config
+from common import utils as util
+from process_go_cardless.go_cardless_report_download import GoCardlessReport
+import sentry_sdk
+from common.slack_utils import alert_slack
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2),  #  don't know what this is doing
+    'on_failure_callback': alert_slack,
+}
+
+dag = DAG(
+    dag_id='go_cardless_report',
+    default_args=args,
+    schedule_interval='00 08 * * *',
+    tags=['cdw'],
+    catchup=False,
+    max_active_runs=1,
+)
+
+def download_go_cardless_report_wrapper(execution_date):
+    """
+    :param: execution_date a string in the form 'YYYY-MM-DD'
+    """
+    try:
+        print("download_go_cardless_report  execution_date={}".format(execution_date))
+        directory = util.get_dir()
+        bucket_name = directory["s3_finance_bucket"]
+        instance = GoCardlessReport(config, bucket_name)
+        instance.process()
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        sentry_sdk.flush(5)
+        raise e
+
+download_go_cardless_report_task = PythonOperator(
+    task_id="download_go_cardless_report",
+    python_callable=download_go_cardless_report_wrapper,
+    op_args=["{{ ds }}"],
+    dag=dag,
+)
+
+download_go_cardless_report_task
