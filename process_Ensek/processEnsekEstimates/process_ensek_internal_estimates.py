ecd93a91d0a24766708133a550029607b1d4bde7
Jonathan Steel <jonathan.steel@igloo.energy>
2021-06-04 10:54:59 +0000

    Merged in feature/MTRG-893 (pull request #148)
    Feature/MTRG-893
    
    * fix: Removed draft test file
    MTRG-893
    
    * Fix: Removed errant test file
    MTRG-893
    
    * feature: Apply correct Logging to internal estimates
    MTRG-889
    
    * fix: Removed draft test file
    MTRG-893
    
    * Fix: Removed errant test file
    MTRG-893
    
    * Merge branch 'feature/MTRG-893' of https://bitbucket.org/iglooenergy/enzek-meterpoint-readings into feature/MTRG-893
    
    * feature: Corrected Unit tests
    MTRG-893
    
    * feature: Removed comment
    MTRG-893
    
    * feature: Corrected distribution of work to multiprocessor
    MTRG-863
    
    * feature: Ensured array size in metrics does get to large
    MTRG-893
    
    Approved-by: Rob Gill
    Approved-by: Alex Geeson

--- process_Ensek/processEnsekEstimates/process_ensek_internal_estimates.py
+++ process_Ensek/processEnsekEstimates/process_ensek_internal_estimates.py
@@ -1,29 +1,33 @@
+import timeit
 import requests
 import json
-from pandas.io.json import json_normalize
+import pandas
 from ratelimit import limits, sleep_and_retry
 import time
-from time import sleep
-import timeit
-import datetime
-from requests import ConnectionError
-import csv
+
 import multiprocessing
 from multiprocessing import freeze_support
+from multiprocessing import Manager, freeze_support, Value
+import statistics
 
 import sys
 import os
 
 sys.path.append('..')
+sys.path.append('../..')
 
-from conf import config as con
-from common import utils as util
+import common
+import common.utils
+from conf import config
 from connections.connect_db import get_boto_S3_Connections as s3_con
+
+iglog = common.utils.IglooLogger(source="EnsekInternalEstimates")
 
 
 class InternalEstimates:
-    max_calls = con.api_config['max_api_calls']
-    rate = con.api_config['allowed_period_in_secs']
+    max_calls = config.api_config['max_api_calls']
+    rate = config.api_config['allowed_period_in_secs']
+
 
     def __init__(self):
         pass
@@ -31,14 +35,15 @@ class InternalEstimates:
 
     @sleep_and_retry
     @limits(calls=max_calls, period=rate)
-    def get_api_response(self, api_url, head):
+    def get_api_response(self, api_url, head, account_id, metrics):
         """
             get the response for the respective url that is passed as part of this function
         """
         session = requests.Session()
         start_time = time.time()
-        timeout = con.api_config['connection_timeout']
-        retry_in_secs = con.api_config['retry_in_secs']
+        timeout = config.api_config['connection_timeout']
+        retry_in_secs = config.api_config['retry_in_secs']
+        attempt_num = 0
         i = 0
         while True:
             try:
@@ -49,54 +54,65 @@ class InternalEstimates:
                     response_items_gas = response_json['Gas']
                     return response_items_elec, response_items_gas
                 else:
-                    print('Problem Grabbing Data: ', response.status_code)
-                    #self.log_error('Response Error: Problem grabbing data', response.status_code)
-                    return None, None
+                    metrics[0]["api_error_codes"].append(response.status_code)
                     break
 
             except ConnectionError:
+                with metrics[0]["connection_error_counter"].get_lock():
+                    metrics[0]["connection_error_counter"].value += 1
                 if time.time() > start_time + timeout:
-                    print('Unable to Connect after {} seconds of ConnectionErrors'.format(timeout))
-                    log_error('Unable to Connect after {} seconds of ConnectionErrors'.format(timeout))
-                    break
+                    try:
+                        with metrics[0]["connection_error_counter"].get_lock():
+                            metrics[0]["connection_error_counter"].value += 1
+                            if metrics[0]["connection_error_counter"].value % 100 == 0:
+                                iglog.in_prod_env(
+                                    "Connection errors: {}".format(
+                                        str(metrics[0]["connection_error_counter"])
+                                    )
+                                )
+                        break
+                    except:
+                        break
                 else:
-                    print('Retrying connection in ' + str(retry_in_secs) + ' seconds' + str(i))
-                    log_error('Retrying connection in ' + str(retry_in_secs) + ' seconds' + str(i))
-
+                    try:
+                        with metrics[0]["number_of_retries_total"].get_lock():
+                            metrics[0]["number_of_retries_total"].value += 1
+                            if attempt_num > 0:
+                                metrics[0]["retries_per_account"][
+                                    account_id
+                                ] = attempt_num
+                    except:
+                        pass
                     time.sleep(retry_in_secs)
             i = i + retry_in_secs
+        method_time = time.time() - start_time
+        metrics[0]["api_method_time"].append(method_time)
 
 
-    def extract_internal_data_response_elec(self, data, account_id, k, dir_s3):
+    def extract_internal_data_response_elec(self, data, account_id, k, dir_s3, metrics):
         ''' Processing meter points data'''
-        # meta_meters = ['associationStartDate', 'associationEndDate', 'supplyStartDate', 'supplyEndDate', 'isSmart', 'isSmartCommunicating', 'id', 'meterPointNumber', 'meterPointType']
-        df_internal_readings = json_normalize(data)
-        # print(df_internal_readings)
+        df_internal_readings = pandas.json_normalize(data)
         if (df_internal_readings.empty):
-            print(" - has no readings data")
+            metrics[0]["no_internal_estimates_elec_data"].append(account_id)
         else:
             df_internal_readings['account_id'] = account_id
-            column_list = util.get_common_info('ensek_column_order', 'internal_estimates_elec')
+            column_list = common.utils.get_common_info('ensek_column_order', 'internal_estimates_elec')
             df_internal_readings_string = df_internal_readings.to_csv(None, columns=column_list, index=False)
             file_name_internal_readings = 'internal_estimates_elec_' + str(account_id) + '.csv'
-            # k.key = 'ensek-meterpoints/EstimatesElecInternal/' + file_name_internal_readings
             k.key = dir_s3['s3_key']['EstimatesElecInternal'] + file_name_internal_readings
             k.set_contents_from_string(df_internal_readings_string)
 
 
-    def extract_internal_data_response_gas(self, data, account_id, k, dir_s3):
+    def extract_internal_data_response_gas(self, data, account_id, k, dir_s3, metrics):
         ''' Processing meter points data'''
-        # meta_meters = ['associationStartDate', 'associationEndDate', 'supplyStartDate', 'supplyEndDate', 'isSmart', 'isSmartCommunicating', 'id', 'meterPointNumber', 'meterPointType']
-        df_internal_readings = json_normalize(data)
-        # print(df_internal_readings)
+        df_internal_readings = pandas.json_normalize(data)
         if (df_internal_readings.empty):
-            print(" - has no readings data")
+            metrics[0]["no_internal_estimates_gas_data"].append(account_id)
         else:
             df_internal_readings['account_id'] = account_id
-            column_list = util.get_common_info('ensek_column_order', 'internal_estimates_gas')
+            column_list = common.utils.get_common_info('ensek_column_order', 'internal_estimates_gas')
             df_internal_readings_string = df_internal_readings.to_csv(None, columns=column_list, index=False)
             file_name_internal_readings = 'internal_estimates_gas_' + str(account_id) + '.csv'
-            # k.key = 'ensek-meterpoints/EstimatesGasInternal/' + file_name_internal_readings
             k.key = dir_s3['s3_key']['EstimatesGasInternal'] + file_name_internal_readings
             k.set_contents_from_string(df_internal_readings_string)
 
@@ -125,101 +141,149 @@ class InternalEstimates:
         return data_json
 
 
-    def log_error(self, error_msg, error_code=''):
-        logs_dir_path = sys.path[0] + '/logs/'
-        if not os.path.exists(logs_dir_path):
-            os.makedirs(logs_dir_path)
-        with open(sys.path[0] + '/logs/' + 'internal_estimates_logs_' + time.strftime('%d%m%Y') + '.csv', mode='a') as errorlog:
-            employee_writer = csv.writer(errorlog, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            employee_writer.writerow([error_msg, error_code])
+    def processAccounts(self, account_ids, k, _dir_s3, metrics):
 
-
-    def processAccounts(self, account_ids, k, _dir_s3):
-
-        api_url, head = util.get_ensek_api_info1('internal_estimates')
+        api_url, head = common.utils.get_ensek_api_info1('internal_estimates')
         for account_id in account_ids:
-            t = con.api_config['total_no_of_calls']
-
-            print('ac:' + str(account_id))
-            msg_ac = 'ac:' + str(account_id)
-            #self.log_error(msg_ac, '')
+            with metrics[0]["account_id_counter"].get_lock():
+                metrics[0]["account_id_counter"].value += 1
+                if metrics[0]["account_id_counter"].value % 1000 == 0:
+                    iglog.in_prod_env(
+                        "Account IDs processesed: {}".format(
+                            str(metrics[0]["account_id_counter"].value)
+                        )
+                    )
             api_url1 = api_url.format(account_id)
-            internal_data_response_elec, internal_data_response_gas = self.get_api_response(api_url1, head)
-            # print(json.dumps(internal_data_response_elec, indent=4))
-
+            internal_data_response_elec, internal_data_response_gas = self.get_api_response(api_url1, head, account_id, metrics)
+            
             if internal_data_response_elec:
                 formatted_internal_data_elec = self.format_json_response(internal_data_response_elec)
-                self.extract_internal_data_response_elec(formatted_internal_data_elec, account_id, k, _dir_s3)
+                self.extract_internal_data_response_elec(formatted_internal_data_elec, account_id, k, _dir_s3, metrics)
 
             if internal_data_response_gas:
                 formatted_internal_data_gas = self.format_json_response(internal_data_response_gas)
-                self.extract_internal_data_response_gas(formatted_internal_data_gas, account_id, k, _dir_s3)
+                self.extract_internal_data_response_gas(formatted_internal_data_gas, account_id, k, _dir_s3, metrics)
 
+            if not internal_data_response_elec and internal_data_response_gas:
+                metrics[0]["accounts_with_no_data"].append(account_id)
 
-if __name__ == "__main__":
+def main():
     freeze_support()
 
-    dir_s3 = util.get_dir()
+    dir_s3 = common.utils.get_dir()
     bucket_name = dir_s3['s3_bucket']
 
     s3 = s3_con(bucket_name)
 
     account_ids = []
     '''Enable this to test for 1 account id'''
-    if con.test_config['enable_manual'] == 'Y':
-        account_ids = con.test_config['account_ids']
+    if config.test_config['enable_manual'] == 'Y':
+        account_ids = config.test_config['account_ids']
 
-    if con.test_config['enable_file'] == 'Y':
-        account_ids = util.get_Users_from_s3(s3)
+    if config.test_config['enable_file'] == 'Y':
+        account_ids = common.utils.get_Users_from_s3(s3)
 
-    if con.test_config['enable_db'] == 'Y':
-        account_ids = util.get_accountID_fromDB(False)
+    if config.test_config['enable_db'] == 'Y':
+        account_ids = common.utils.get_accountID_fromDB(False)
 
-    if con.test_config['enable_db_max'] == 'Y':
-        account_ids = util.get_accountID_fromDB(True)
+    if config.test_config['enable_db_max'] == 'Y':
+        account_ids = common.utils.get_accountID_fromDB(True)
 
-    # Enable to test without multiprocessing.
-    # p = InternalEstimates()
-    # p.processAccounts(account_ids, s3, dir_s3)
 
-    #     ####### Multiprocessing Starts #########
-    env = util.get_env()
-    total_processes = util.get_multiprocess('total_ensek_processes')
-
-    if env == 'uat':
-        n = total_processes  # number of process to run in parallel
-    else:
-        n = total_processes
-    k = int(len(account_ids) / n)  # get equal no of files for each process
-
-    print(len(account_ids))
-    print(k)
-
+    ####### Multiprocessing Starts #########
+    total_processes = common.utils.get_multiprocess('total_ensek_processes')
+    accounts_per_process = int(len(account_ids) / total_processes)
+    iglog.in_prod_env(
+        "Number of account IDs to be processed per thread -  {}".format(
+            "{:.2f}".format(len(account_ids) / total_processes)
+        )
+    )
     processes = []
-    lv = 0
+    last_value = 0
     start = timeit.default_timer()
 
-    for i in range(n + 1):
-        p1 = InternalEstimates()
-        print(i)
-        uv = i * k
-        if i == n:
-            # print(d18_keys_s3[l:])
-            t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:], s3_con(bucket_name), dir_s3))
-        else:
-            # print(d18_keys_s3[l:u])
-            t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[lv:uv], s3_con(bucket_name), dir_s3))
-        lv = uv
+    # Raw Metrics
+    with Manager() as manager:
 
-        processes.append(t)
+        metrics = {
+            "api_error_codes": manager.list(),
+            "connection_error_counter": Value("i", 0),
+            "number_of_retries_total": Value("i", 0),
+            "retries_per_account": manager.dict(),
+            "api_method_time": manager.list(),
+            "no_internal_estimates_elec_data": manager.list(),
+            "no_internal_estimates_gas_data": manager.list(),
+            "account_id_counter": Value("i", 0),
+            "accounts_with_no_data": manager.list(),
+        }
 
-    for p in processes:
-        p.start()
-        sleep(2)
+        try:
+            for i in range(1, total_processes + 1):
+                p1 = InternalEstimates()
+                uv = i * accounts_per_process
+                if i == total_processes:
+                    t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[last_value:], s3_con(bucket_name), dir_s3, [metrics]))
+                else:
+                    t = multiprocessing.Process(target=p1.processAccounts, args=(account_ids[last_value:uv], s3_con(bucket_name), dir_s3, [metrics]))
+                last_value = uv
 
-    for process in processes:
-        process.join()
-    ####### Multiprocessing Ends #########
+                processes.append(t)
 
-    print("Process completed in " + str(timeit.default_timer() - start) + ' seconds')
+            for process in processes:
+                process.start()
+                time.sleep(2)
 
+            for process in processes:
+                process.join()
+            ###### Multiprocessing Ends #########
+        finally:
+            start_metrics = timeit.default_timer()
+            ####### Multiprocessing Ends #########
+            if len(metrics["api_method_time"]) != 0:
+                metrics["max_api_process_time"] = max(metrics["api_method_time"])
+                metrics["min_api_process_time"] = min(metrics["api_method_time"])
+                metrics["median_api_process_time"] = statistics.median(
+                    metrics["api_method_time"]
+                )
+                metrics["average_api_process_time"] = statistics.mean(
+                    metrics["api_method_time"]
+                )
+            else:
+                metrics["max_api_process_time"] = "No api times processed"
+                metrics["min_api_process_time"] = "No api times processed"
+                metrics["median_api_process_time"] = "No api times processed"
+                metrics["average_api_process_time"] = "No api times processed"
+
+            metrics["connection_error_counter"] = metrics[
+                "connection_error_counter"
+            ].value
+            metrics["number_of_retries_total"] = metrics[
+                "number_of_retries_total"
+            ].value
+            metrics["account_id_counter"] = metrics["account_id_counter"].value
+
+            iglog.in_prod_env(
+                "Process completed in "
+                + str(timeit.default_timer() - start)
+                + " seconds\n"
+            )
+            iglog.in_prod_env(
+                "Metrics completed in "
+                + str(timeit.default_timer() - start_metrics)
+                + " seconds\n"
+            )
+            iglog.in_prod_env("METRICS FROM CURRENT RUN...\n")
+            for metric_name, metric_data in metrics.items():
+                if metric_name in [
+                    "api_method_time",
+                ]:
+                    continue
+                if isinstance(metric_data, multiprocessing.managers.ListProxy) and len(metric_data) >= 50:
+                    metric_data = len(metric_data)
+                iglog.in_prod_env(
+                    str(metric_name).upper() + "\n" + str(metric_data) + "\n"
+                )
+
+            
+if __name__ == "__main__":
+    main()
