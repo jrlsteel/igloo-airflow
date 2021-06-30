18eb96c4110bf0a7bba30550e73dcde957c7b0ab
Alex Geeson <alex.geeson@igloo.energy>
2021-06-30 15:57:35 +0000

    Merged in feature/MTRG-1001 (pull request #165)
    Feature/MTRG-1001
    
    * feat: slack alert for daily_reporting
    
    MTRG-1001
    
    * feat: changed channel depending on environment
    
    * feat: create slack message function
    
    * feat: added slack messages for each calculated step job
    
    * chore: resolved review comments
    
    * feat: added secrets manager to store slack token
    
    * feat: added env specific channels
    
    * chore: cleanup
    
    * feat: updated messages and logging
    
    Approved-by: Gareth John
    Approved-by: Ross Burchnall

--- process_calculated_steps/start_calculated_steps_jobs.py
+++ process_calculated_steps/start_calculated_steps_jobs.py
@@ -13,6 +13,7 @@ from process_data_gate import start_smart_meter_eligibility_jobs as sme, start_m
 from process_Ensek.processEnsekTariffs import start_igloo_calculated_tariffs_job as calc_tariffs
 
 from common import utils as util
+from common import slack_utils
 
 
 class CalcSteps:
@@ -37,31 +38,43 @@ class CalcSteps:
         print("{0}: EAC and AQ Glue V1 Job running...".format(datetime.now().strftime("%H:%M:%S")))
         iglindeacaq_obj = iglindeacaq.IglIndEacAq()
         iglindeacaq_obj.submit_eac_aq_gluejob()
+        slack_utils.post_slack_message(
+            """Task Complete: EAC and AQ Calculations :white_check_mark:""", "cdw-daily-reporting"
+        )
 
         # run consumption accuracy job
         print("{0}: Consumption Accuracy Job running...".format(datetime.now().strftime("%H:%M:%S")))
         ca_obj = ca.ConsumptionAccuracy()
         ca_obj.submit_consumption_accuracy_gluejob()
+        slack_utils.post_slack_message(
+            """Task Complete: Consumption Accuracy :white_check_mark:""", "cdw-daily-reporting"
+        )
 
         # # run TADO efficiency job
         print("{0}: TADO Efficiency Job running...".format(datetime.now().strftime("%H:%M:%S")))
         ta_obj = ta.TADOEfficiencyJobs()
         ta_obj.submit_tado_efficiency_batch_gluejob()
+        slack_utils.post_slack_message("""Task Complete: TADO Efficiency :white_check_mark:""", "cdw-daily-reporting")
 
         # run daily sales job
         print("{0}: Daily Reporting Job running...".format(datetime.now().strftime("%H:%M:%S")))
         rj_obj = srj.ReportingJobs()
         rj_obj.submit_daily_reporting_batch_gluejob()
+        slack_utils.post_slack_message(
+            """Task Complete: Daily Reports: DMF, DCF :white_check_mark:""", "cdw-daily-reporting"
+        )
 
         # run Estimated Advance Job
         print("{0}: Estimated Advance Job running...".format(datetime.now().strftime("%H:%M:%S")))
         est_adv_obj = est_adv.EstimatedAdvance()
         est_adv_obj.submit_estimated_advance_gluejob()
+        slack_utils.post_slack_message("""Task Complete: Estimated Advance :white_check_mark:""", "cdw-daily-reporting")
 
         # run eligibility jobs
         print("{0}: Eligibility Reporting Job running...".format(datetime.now().strftime("%H:%M:%S")))
         rj_obj = srj.ReportingJobs()
         rj_obj.submit_eligibility_reporting_batch_gluejob()
+        slack_utils.post_slack_message("""Task Complete: Eligibility :white_check_mark:""", "cdw-daily-reporting")
 
         print("{0}: All {1} completed successfully".format(datetime.now().strftime("%H:%M:%S"), self.process_name))
         util.batch_logging_update(self.all_jobid, "e")
