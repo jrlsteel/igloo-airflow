238589160a7ccafccfdd9e68bb7bc929cca5f7e3
Alex Geeson <alex.geeson@igloo.energy>
2021-09-17 09:03:11 +0100

    fix: broken dag

--- dags/ensek_am-DAG.py
+++ dags/ensek_am-DAG.py
@@ -322,5 +322,5 @@ for datatype, datatype_info in dag_data.items():
             mirror_task_string = f"mirroring_{datatype}_{prefix}"
             tasks[mirror_task_string] = createMirrorTask(mirror_task_string, prefix)
             tasks[staging_task_string].set_upstream(tasks[mirror_task_string])
-            tasks[api_verify_string].set_upsream(tasks[mirror_task_string])
+            tasks[api_verify_string].set_upstream(tasks[mirror_task_string])
             # tasks[mirror_task_string].set_upstream(sensor)
